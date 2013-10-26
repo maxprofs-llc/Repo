84592ea414da4ba5df34a9837fd8edb17de50320
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-10-26 03:09:51 +0000

    score edit

--- Flipper/SM/flippersm.se/classes/score.class.php
+++ Flipper/SM/flippersm.se/classes/score.class.php
@@ -191,6 +191,8 @@
 
     function setGame($dbh, $game = null) {
       $machine = ($game) ? $game->getMachine($dbh, $this->tournamentDivision_id) : null;
+      var_dump($machine);
+      die('hej');
       $this->game_id = ($game) ? $game->id : null;
       $this->game = ($game) ? $game->name : null;
       $this->gameAcronym = ($game) ? $game->shortName : null;
