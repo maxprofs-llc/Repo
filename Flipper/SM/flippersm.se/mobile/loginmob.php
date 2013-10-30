48f7a33f98ac4f5e004f0385b59eeaadc25f70a0
bitwalk <bitwalk@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-10-30 22:07:31 +0000

    - Updated login.php to M class prefix

--- Flipper/SM/flippersm.se/mobile/loginmob.php
+++ Flipper/SM/flippersm.se/mobile/loginmob.php
@@ -2,14 +2,14 @@
   require_once('../functions/general.php');
   require_once('mobile.php');
 
-  $oHTTPContext = new HTTPContext();
+  $oHTTPContext = new MHTTPContext();
 
   $sUsername = $oHTTPContext->getString("user");
   $sPassword = $oHTTPContext->getString("password");
 
   if($sUsername != null && $sPassword != null)
   {
-    $oUser = new User();
+    $oUser = new MUser();
     if(!$oUser->logIn($sUsername, $sPassword)){
       echo "statusCode=1";
     } else {
