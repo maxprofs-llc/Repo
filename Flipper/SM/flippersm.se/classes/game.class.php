1711a2941bbdc41cce326a4fda856c5436cf363a
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-09 07:11:40 +0000

    'showScores'

--- Flipper/SM/flippersm.se/classes/game.class.php
+++ Flipper/SM/flippersm.se/classes/game.class.php
@@ -545,7 +545,6 @@
         foreach ($entries as $entry) {
           $content .= '
             <div id="'.(($division == 1) ? 'main' : (($division == 2) ? 'classics' : 'team')).'Table" class="section" style="display: '.(($_REQUEST['active']) ? '' : '').';">
-            <p>'.$this->name.' har entry ID '.$entry->id.(($entry->points) ? ' med <span title="'.$entry->points.'">'.round($entry->points).'</span> poäng' : '').(($entry->place) ? ' på plats '.$entry->place : '').'</p>
           ';
         }
       }
