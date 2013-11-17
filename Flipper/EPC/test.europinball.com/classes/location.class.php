9ea54fc999908b3ee7953a78500f0bfd7f6e6546
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-17 21:11:17 +0000

    tournament'

---
+++ Flipper/EPC/test.europinball.com/classes/location.class.php
@@ -0,0 +1,25 @@
+<?php
+
+  class location extends geography {
+        
+    public static $select = '
+      select 
+        o.id as id,
+        o.streetAddress as streetAddress,
+        o.zipCode as zipCode,
+        o.city_id as city_id,
+        o.region_id as region_id,
+        o.parentRegion_id as parentRegion_id,
+        o.country_id as country_id,
+        o.parentCountry_id as parentCountry_id,
+        o.continent_id as continent_id,
+        o.latitude as latitude,
+        o.longitude as longitude,
+        o.comment as comment
+      from location o
+    ';
+    
+
+  }
+
+?>
