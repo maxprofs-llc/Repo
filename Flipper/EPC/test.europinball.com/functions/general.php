b634de1900cc95bcef7c669198e9ba7c59e040c0
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-17 20:30:46 +0000

    tournament

---
+++ Flipper/EPC/test.europinball.com/functions/general.php
@@ -0,0 +1,9 @@
+<?php
+
+  spl_autoload_register(function($class) {
+    if (is_file(__ROOT__.'/classes/'.$class.'.class.php')) {
+      include __ROOT__.'/classes/'.$class.'.class.php';
+    }
+  });
+
+?>
