e65eb8efdfa77c9462f4b25098641aed6a0a9d7c
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-18 03:07:53 +0000

    tournament'

--- Flipper/EPC/test.europinball.com/classes/geography.class.php
+++ Flipper/EPC/test.europinball.com/classes/geography.class.php
@@ -2,26 +2,5 @@
 
   abstract class geography extends base {
     
-    public $streetAddress;
-    public $zipCode;
-    public $continent_id;
-    public $continent;
-    public $parentCountry_id;
-    public $parentCountry;
-    public $country_id;
-    public $country;
-    public $parentRegion_id;
-    public $parentRegion;
-    public $region_id;
-    public $region;
-    public $city_id;
-    public $city;
-    public $altName;
-    public $capitalCity_id;
-    public $capitalCity;
-    public $latitude;
-    public $longitude;
-    public $selfParent = false;
-    
   }
 ?>
