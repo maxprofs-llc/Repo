30fd3574ea631f590d4c47516d6ba53b9110fda1
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-18 01:12:54 +0000

    tournament'

--- Flipper/EPC/test.europinball.com/classes/db.class.php
+++ Flipper/EPC/test.europinball.com/classes/db.class.php
@@ -20,14 +20,15 @@
       if (array_key_exists('ID'.$id, $class::$instances)) {
         return $class::$instances['ID'.$id];
       } else {
-      $query = $class::$select.' where o.id = '.$id;
-      $this->sth = $this->query($query);
-      $obj = $this->sth->fetchObject($class);
-      if ($this->last_row_count()) {
-        $class::$instances['ID'.$id] = $obj;
-        return $obj;
-      } else {
-        return FALSE;
+        $query = $class::$select.' where o.id = '.$id;
+        $this->sth = $this->query($query);
+        $obj = $this->sth->fetchObject($class);
+        if ($this->last_row_count()) {
+          $class::$instances['ID'.$id] = $obj;
+          return $obj;
+        } else {
+          return FALSE;
+        }
       }
     }
 
