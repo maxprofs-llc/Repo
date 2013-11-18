7fa22e3274d3799c9e6c3b52cddb32e43dc646a2
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-18 21:48:37 +0000

    obj classes

--- Flipper/EPC/test.europinball.com/tournament.php
+++ Flipper/EPC/test.europinball.com/tournament.php
@@ -14,6 +14,6 @@
   $tournament = new tournament(1);
 //  $divisions = $tournament->getDivisions();
   
-  pre_dump($tournament->location->city->region);
+  pre_dump($tournament->location->city->region->continent);
 
 ?>
