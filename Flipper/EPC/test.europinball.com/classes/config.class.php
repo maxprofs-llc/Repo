d5368abdc3f0cb6f15c874cc5e1ef121a50755a3
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-18 20:33:44 +0000

    obj classes

---
+++ Flipper/EPC/test.europinball.com/classes/config.class.php
@@ -0,0 +1,5 @@
+<?php
+
+  public static $depth = 1;
+
+?>
--- Flipper/EPC/test.europinball.com/tournament.php
+++ Flipper/EPC/test.europinball.com/tournament.php
@@ -2,7 +2,15 @@
 
   define('__ROOT__', dirname(__FILE__)); 
   require_once(__ROOT__.'/functions/general.php');
+
+/*  
+  $country = new country(188);
+  $locations = $country->getLocations();
   
+  pre_dump($locations);
+*/
+
+
   $tournament = new tournament(1);
   $divisions = $tournament->getDivisions();
   
