1c60d5acd5a72c77ab78e50b436151b81e1a6c6f
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-18 03:42:00 +0000

    tournament'

--- Flipper/EPC/test.europinball.com/classes/db.class.php
+++ Flipper/EPC/test.europinball.com/classes/db.class.php
@@ -26,7 +26,6 @@
         $query = $class::$select.' where o.id = '.$id;
         $this->sth = $this->query($query);
         $obj = $this->sth->fetchObject($class);
-        unset($obj->db->queryString);
         unset($obj->db->sth);
         if ($this->last_row_count()) {
           $class::$instances['ID'.$id] = $obj;
