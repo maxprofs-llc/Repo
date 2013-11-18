3320ffebc674e57f822c6f5f473b4b7f93bbef7f
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-18 04:23:48 +0000

    tournament

--- Flipper/EPC/test.europinball.com/classes/db.class.php
+++ Flipper/EPC/test.europinball.com/classes/db.class.php
@@ -38,7 +38,6 @@
       $column = ($column) ? $column : get_class($parent).'_id';
       $parentClass = get_class($parent);
       $query = $class::$select.' where o.'.$column.' = '.$parent->id.(($parentClass::$selfParent) ? ' or parent'.ucfirst($column).' = '.$parent->id : '');
-      echo $query;
       $sth = $this->query($query);
       while($obj = $sth->fetchObject($class)) {
         $class::$instances['ID'.$id] = $obj;
