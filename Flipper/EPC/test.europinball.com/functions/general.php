6cb33fb5138edfb21d79996b5aa1659c446355cf
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-19 17:29:50 +0000

    db

--- Flipper/EPC/test.europinball.com/functions/general.php
+++ Flipper/EPC/test.europinball.com/functions/general.php
@@ -7,8 +7,12 @@
   });
   
   function isAssoc(&$arr) {
-    for (reset($arr); is_int(key($arr)); next($arr));
-    return !(is_array($arr) && is_null(key($arr)));
+    if (is_array($arr)) {
+      for (reset($arr); is_int(key($arr)); next($arr));
+      return !is_null(key($arr));
+    } else {
+      return false;
+    }
   }
 
   function preDump($obj) {
