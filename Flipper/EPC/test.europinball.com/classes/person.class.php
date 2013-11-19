4cc07761cd7e99e0bd327a14752b8617287635fe
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-19 19:14:01 +0000

    db

--- Flipper/EPC/test.europinball.com/classes/person.class.php
+++ Flipper/EPC/test.europinball.com/classes/person.class.php
@@ -33,7 +33,7 @@
         ifnull(v.alloc, 0) as alloc,
         timediff(time(concat(ifnull(v.hours, "00"), ":00:00")), ifnull(v.alloc, time("00:00:00"))) as hoursDiff,
         o.username as username
-      from person p
+      from person o
       left join volunteer v
         on v.person_id = o.id and v.tournamentEdition_id = 1
     ';
