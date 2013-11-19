65d0fd1878017f68cc12338bff19ac983910a477
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-19 18:49:13 +0000

    db

--- Flipper/EPC/test.europinball.com/classes/db.class.php
+++ Flipper/EPC/test.europinball.com/classes/db.class.php
@@ -100,12 +100,13 @@
     
     protected function getObjectsByProps($class, $props, $cond = 'and') {
       foreach ($props as $key => $value) {
+        $key = (preg_match('/\./', $key)) ? $key : 'o.'.$key; 
         if ($where) {
-          $query .= ' '.$cond.' '.$key.' = :'.str_replace('.', '', $key);
-          $values[':'.str_replace('.', '', $key)] = $value;
+          $query .= ' '.$cond.' '.$key.' = :'.preg_replace('[^a-zA-Z0-9_]', '', $key);
+          $values[':'.preg_replace('[^a-zA-Z0-9_]', '', $key)] = $value;
         } else {
-          $query = $class::$select.' where '.$key.' = :'.str_replace('.', '', $key);
-          $values = array(':'.str_replace('.', '', $key) => $value);
+          $query = $class::$select.' where '.$key.' = :'.preg_replace('[^a-zA-Z0-9_]', '', $key);
+          $values = array(':'.preg_replace('[^a-zA-Z0-9_]', '', $key) => $value);
           $where = true;
         }
       }
