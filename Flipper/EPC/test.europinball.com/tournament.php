4297931758575d5accf392e565f062acf62af774
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-20 15:37:55 +0000

    db

--- Flipper/EPC/test.europinball.com/tournament.php
+++ Flipper/EPC/test.europinball.com/tournament.php
@@ -3,24 +3,19 @@
   define('__ROOT__', dirname(__FILE__)); 
   require_once(__ROOT__.'/functions/general.php');
 
-/*  
-  $country = new country(188);
-  $locations = $country->getLocations();
+//  $country = new country(188);
+//  $obj = $country->getLocations();
   
-  preDump($locations);
-*/
-
-/*
-  $tournament = new tournament(1);
-//  $divisions = $tournament->getDivisions();
+//  $tournament = new tournament(1);
+//  $obj = $tournament->getDivisions();
   
-  preDump($tournament->location->city->region->continent);
-*/
+//$obj = new person(array('ifpa_id' => '27', 'mailAddress' => 'the@pal.pp.se'), true);
+//$obj = new person(array('ifpa_id' => '27', 'mailAddress' => 'the@pal.pp.se'), true);
 
-//$player = new person(array('ifpa_id' => '27', 'mailAddress' => 'the@pal.pp.se'), true);
-//$player = new person(array('ifpa_id' => '27', 'mailAddress' => 'the@pal.pp.se'), true);
-$player = new person('27', 'ifpa_id');
+$obj = new person('27', 'ifpa_id');
+//$obj = new player('27', 'ifpa_id');
+
 echo 'hej';
-preDump($player);
+preDump($obj);
 
 ?>
