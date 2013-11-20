56ac2db68ed1185ba6b63ed4d63767caf2903271
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-20 15:40:37 +0000

    db

--- Flipper/EPC/test.europinball.com/tournament.php
+++ Flipper/EPC/test.europinball.com/tournament.php
@@ -10,10 +10,10 @@
 //  $obj = $tournament->getDivisions();
   
 // $obj = new person(array('ifpa_id' => '27', 'mailAddress' => 'the@pal.pp.se'), true);
-// $obj = new player(array('ifpa_id' => '27', 'mailAddress' => 'the@pal.pp.se'), true);
+$obj = new player(array('ifpa_id' => '27', 'mailAddress' => 'the@pal.pp.se'), true);
 
 // $obj = new person('27', 'ifpa_id');
-$obj = new player('27', 'ifpa_id');
+// $obj = new player('27', 'ifpa_id');
 
 echo 'hej';
 preDump($obj);
