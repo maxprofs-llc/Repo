1c28fd0fd891397586912deb79e40a9b4221de9b
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-21 02:15:53 +0000

    db

--- Flipper/EPC/test.europinball.com/classes/group.class.php
+++ Flipper/EPC/test.europinball.com/classes/group.class.php
@@ -2,7 +2,7 @@
 
   abstract class group extends ArrayObject {
     
-    public static $type = 'group';
+    public static $objClass = 'group';
     
     public function __construct($data = NULL, $prop = NULL, $cond = 'and') {
       if (!base::$_db) {
@@ -10,15 +10,15 @@
       } 
       $this->db = base::$_db;
       if (isAssoc($data)) {
-        $objs = $this->db->getObjectsByProps(static::$type, $data, $cond);
+        $objs = $this->db->getObjectsByProps(static::$objClass, $data, $cond);
       } else if (is_object($data) && $data->id && is_string($prop)) {
-        $objs = $this->db->getObjectsByProp(static::$type, $prop, $data->id);
+        $objs = $this->db->getObjectsByProp(static::$objClass, $prop, $data->id);
       } else if (is_object($data) && $data->id) {
-        $objs = $this->db->getObjectsByProp(static::$type, get_class($data).'_id', $data->id);
+        $objs = $this->db->getObjectsByProp(static::$objClass, get_class($data).'_id', $data->id);
       } else if ($data && is_string($prop)) {
-        $objs = $this->db->getObjectsByProp(static::$type, $prop, $data);
+        $objs = $this->db->getObjectsByProp(static::$objClass, $prop, $data);
       } else if (is_string($data) && preg_match('/^where /', trim($data))) {
-        $objs = $this->db->getObjectsByWhere(static::$type, $data);
+        $objs = $this->db->getObjectsByWhere(static::$objClass, $data);
       }
       parent::__construct();
       if ($objs) {
