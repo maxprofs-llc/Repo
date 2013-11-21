1ea61d4970c63a803734e59a0fb6a24151aa618b
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-21 00:42:14 +0000

    db

--- Flipper/EPC/test.europinball.com/classes/group.class.php
+++ Flipper/EPC/test.europinball.com/classes/group.class.php
@@ -24,27 +24,5 @@
       }
     }
 
-/*
-    public function offsetSet($index, $value) {
-      if (is_null($index)) {
-        $this->objects[] = $value;
-      } else {
-        $this->objects[$index] = $value;
-      }
-    }
-
-    public function offsetExists($index) {
-        return isset($this->objects[$index]);
-    }
-
-    public function offsetUnset($index) {
-        unset($this->objects[$index]);
-    }
-
-    public function offsetGet($index) {
-        return isset($this->objects[$index]) ? $this->objects[$index] : null;
-    }
-*/
-    
   }
 ?>
