9884ffb454111231a93b203fdc7512e28bb9f123
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-21 02:17:07 +0000

    db

--- Flipper/EPC/test.europinball.com/classes/players.class.php
+++ Flipper/EPC/test.europinball.com/classes/players.class.php
@@ -7,29 +7,29 @@
     public function __construct($data = NULL, $prop = NULL, $cond = 'and') {
       if (!$prop && (is_string($data) || is_int($data))) {
         if(preg_match('/@/',$data)) {
-          $data = (static::$objType == 'player') ? array(
+          $data = (static::$objClass == 'player') ? array(
             'o.mailAddress' => trim($data),
             'p.mailAddress' => trim($data)
           ) : array('o.mailAddress' => trim($data));
           $cond = 'or';
         } else if (preg_match('/^[0-9]{1,5}$/', $data)) {
-          $prop = (static::$objType == 'player') ? 'p.ifpa_id' : 'ifpa_id';
+          $prop = (static::$objClass == 'player') ? 'p.ifpa_id' : 'ifpa_id';
           $data = trim($data);
         } else if (preg_match('/^[0-9 \-\+\(\)]{6,}$/',$data)) {
           $where = 'where replace(replace(replace(replace(replace(o.telephoneNumber," ",""),")",""),")",""),"-",""),"+","") like "%'.preg_replace('/[^0-9]/','',$data).'%"';
           $where .= ' or replace(replace(replace(replace(replace(o.mobileNumber," ",""),")",""),")",""),"-",""),"+","") like "%'.preg_replace('/[^0-9]/','',$data).'%"';
-          $where .= (static::$objType == 'player') ? ' or replace(replace(replace(replace(replace(p.telephoneNumber," ",""),")",""),")",""),"-",""),"+","") like "%'.preg_replace('/[^0-9]/','',$data).'%"' : '';
-          $where .= (static::$objType == 'player') ? ' or replace(replace(replace(replace(replace(p.mobileNumber," ",""),")",""),")",""),"-",""),"+","") like "%'.preg_replace('/[^0-9]/','',$data).'%"' : '';
+          $where .= (static::$objClass == 'player') ? ' or replace(replace(replace(replace(replace(p.telephoneNumber," ",""),")",""),")",""),"-",""),"+","") like "%'.preg_replace('/[^0-9]/','',$data).'%"' : '';
+          $where .= (static::$objClass == 'player') ? ' or replace(replace(replace(replace(replace(p.mobileNumber," ",""),")",""),")",""),"-",""),"+","") like "%'.preg_replace('/[^0-9]/','',$data).'%"' : '';
           $data = $where;
         } else if (preg_match('/^[a-zA-Z0-9 \-]{3}$/',$data)) {
-          $data = (static::$objType == 'player') ? array(
+          $data = (static::$objClass == 'player') ? array(
             'o.initials' => trim($data),
             'p.initials' => trim($data)
           ) : array('o.initials' => trim($data));
           $cond = 'or';
         } else {
           $where = 'where concat(ifnull(o.firstName,""), " ", ifnull(o.lastName,"")) like "%'.trim($data).'%"';
-          $where .= (static::$objType == 'player') ? ' or concat(ifnull(p.firstName,""), " ", ifnull(p.lastName,"")) like "%'.trim($data).'%"' : '';
+          $where .= (static::$objClass == 'player') ? ' or concat(ifnull(p.firstName,""), " ", ifnull(p.lastName,"")) like "%'.trim($data).'%"' : '';
           $data = $where;
         }
       }
