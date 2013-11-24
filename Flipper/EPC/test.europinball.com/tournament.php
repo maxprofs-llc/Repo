6c98f3442c598897834c2b4c81661762fbae9881
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-24 00:00:50 +0000

    players

--- Flipper/EPC/test.europinball.com/tournament.php
+++ Flipper/EPC/test.europinball.com/tournament.php
@@ -19,7 +19,7 @@
 // $obj = new player('27', 'ifpa_id');
  $objs = new player(1645);
  
-// echo "P".property_exists($objs, '$arrClass');
+ echo "P".property_exists($objs, '$arrClass');
 // echo " P2".property_exists($objs, 'arrClass');
 // echo " A".$objs->arrClass;
  echo " A2".player::$arrClass;
