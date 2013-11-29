43325fd85ea5dc30e12c99dc05f70c3e71363f01
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-29 05:31:54 +0000

    auth

--- Flipper/EPC/test.europinball.com/classes/base.class.php
+++ Flipper/EPC/test.europinball.com/classes/base.class.php
@@ -9,6 +9,8 @@
     public static $parentDepth = 0;
 
     public function __construct($data = NULL, $search = NULL) {
+        preDump($data);
+        die('huff');
       if (!self::$_db) {
         self::$_db = new db();
       } 
@@ -18,8 +20,6 @@
       }
       if ($search) {
         if (isAssoc($data)) {
-        preDump($data);
-        die('huff');
           $obj = $this->db->getObjectByProps(get_class($this), $data);
         } else if ($data) {
           $obj = $this->db->getObjectByProp(get_class($this), $search, $data);
