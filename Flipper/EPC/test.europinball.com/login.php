89dc0853b8852de32aed27c0148ac57cf656207a
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-11-30 02:44:10 +0000

    auth

--- Flipper/EPC/test.europinball.com/login.php
+++ Flipper/EPC/test.europinball.com/login.php
@@ -1,3 +1,17 @@
 <?php
 
+$page = new page('Login', true);
+
+if ($_REQUEST['action']) {
+  $page->login->action();
+}
+
+$login = $page->reqLogin('Please provide your credentials', true);
+
+if ($login) {
+  $page->content = 'You are already logged in!';
+}
+
+$page->submit(FALSE, TRUE);
+
 ?>
