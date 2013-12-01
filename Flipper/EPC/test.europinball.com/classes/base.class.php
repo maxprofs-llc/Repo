d2429e1c4b8477d95db1efb4cff4bd1712a457d2
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-01 04:32:14 +0000

    register

--- Flipper/EPC/test.europinball.com/classes/base.class.php
+++ Flipper/EPC/test.europinball.com/classes/base.class.php
@@ -11,7 +11,7 @@
 
     public function __construct($data = NULL, $search = NULL) {
                 self::$count++;
-              if (self::$count > 2) {
+              if (self::$count > 1) {
                 debug($this);
                 debug(self::$count);
                 die('hepp: '.get_class($this));
