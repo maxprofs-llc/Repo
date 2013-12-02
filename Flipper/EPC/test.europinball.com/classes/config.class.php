ecc68b13040a6b80c2825a3e584378577284a6b8
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-02 03:50:01 +0000

    register¨

--- Flipper/EPC/test.europinball.com/classes/config.class.php
+++ Flipper/EPC/test.europinball.com/classes/config.class.php
@@ -11,26 +11,26 @@
     
     // The below will not be used if $dbconfig is set to TRUE
     public static $parentDepth = 1;  // The depth for automatic population of parent objects.
-    public static $activeTournament = 1;
+    public static $activeTournament = 5;
  
     public static $main = TRUE;
-    public static $mainDivision = 1212;
-    public static $mainQualLimit = 50;
+    public static $mainDivision = 15;
+    public static $mainQualLimit = FALSE;
  
-    public static $classics = TRUE;
-    public static $classicsDivision = 2;
-    public static $classicsQualLimit = 32;
+    public static $classics = FALSE;
+    public static $classicsDivision = FALSE;
+    public static $classicsQualLimit = FALSE;
  
-    public static $eighties = FALSE;
-    public static $eightiesDivision = FALSE;
+    public static $eighties = TRUE;
+    public static $eightiesDivision = 16;
     public static $eightiesQualLimit = FALSE;
  
-    public static $team = TRUE;
-    public static $teamDivision = 3;
+    public static $team = FALSE;
+    public static $teamDivision = FALSE;
     public static $teamQualLimit = FALSE;
  
     public static $nationalTeam = TRUE;
-    public static $nationalTeamDivision = 12;
+    public static $nationalTeamDivision = 17;
     public static $nationalTeamQualLimit = FALSE;
 
     public static $baseHref = 'https://test.europinball.org/';
