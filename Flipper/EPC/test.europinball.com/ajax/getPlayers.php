85c5f308cbd4e5defd711f3b2946cac8d2bf9465
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-02 05:07:34 +0000

    register¨'

--- Flipper/EPC/test.europinball.com/ajax/getPlayers.php
+++ Flipper/EPC/test.europinball.com/ajax/getPlayers.php
@@ -3,7 +3,7 @@
   define('__ROOT__', dirname(dirname(__FILE__))); 
   require_once(__ROOT__.'/functions/init.php');
 
-   $search = (isset($_REQUEST['search']) ? $_REQUEST['type'] : null;
+   $search = (isset($_REQUEST['search'])) ? $_REQUEST['type'] : null;
    
    if ($search) {
      $persons = persons($search);
