049c8fa607905d55160da9c7ca0d66ae9adf06ce
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-02 05:19:11 +0000

    register¨'

--- Flipper/EPC/test.europinball.com/ajax/getPlayers.php
+++ Flipper/EPC/test.europinball.com/ajax/getPlayers.php
@@ -3,22 +3,34 @@
   define('__ROOT__', dirname(dirname(__FILE__))); 
   require_once(__ROOT__.'/functions/init.php');
 
-   $search = (isset($_REQUEST['search'])) ? $_REQUEST['search'] : null;
+  $search = (isset($_REQUEST['search'])) ? $_REQUEST['search'] : null;
    
-   echo "hej".search;
+  echo "hej".search;
    
-   if ($search) {
-     $persons = persons($search);
-     if ($persons) {
-       $json = (object) array(
-         'sEcho' => $_REQUEST['sEcho'],
-         'iTotalRecords' => count($persons),
-         'iTotalDisplayRecords' => count($persons),
-       );
-     }
-     debug($json);
-   }
-   
-
+  if ($search) {
+    $persons = persons($search);
+    if ($persons) {
+      $json = (object) array(
+        'sEcho' => $_REQUEST['sEcho'],
+        'iTotalRecords' => count($persons),
+        'iTotalDisplayRecords' => count($persons),
+      );
+    }
+    foreach ($persons as $person) {
+      $json->aaData[] = array(
+        $person->name,
+        $person->shortName,
+        $person->cityName,
+        $person->regionName,
+        $person->countryName,
+        $person->getLink('ifpa'),
+        $person->getLink('photo'),
+        '<input type="button" id="'.$person->id.'_isMe">'
+      );
+    }
+    debug($json);
+  } else {
+   echo '{"aaData": []}';
+  }
 
 ?>
