574db18ad817f7fc9693c12aa12419e64b16c1cd
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-02 05:35:02 +0000

    register¨'

--- Flipper/EPC/test.europinball.com/ajax/getPlayers.php
+++ Flipper/EPC/test.europinball.com/ajax/getPlayers.php
@@ -4,33 +4,36 @@
   require_once(__ROOT__.'/functions/init.php');
 
   $search = (isset($_REQUEST['search'])) ? $_REQUEST['search'] : null;
-   
-  echo "hej".search;
+  $type = (isset($_REQUEST['type']) && ($_REQUEST['type'] == 'regSearch' || $_REQUEST['type'] == 'scores')) ? $_REQUEST['type'] : null;
    
   if ($search) {
     $persons = persons($search);
-    if ($persons) {
-      $json = (object) array(
-        'sEcho' => $_REQUEST['sEcho'],
-        'iTotalRecords' => count($persons),
-        'iTotalDisplayRecords' => count($persons),
-      );
-    }
-    foreach ($persons as $person) {
-      $json->aaData[] = array(
-        $person->name,
-        $person->shortName,
-        $person->cityName,
-        $person->regionName,
-        $person->countryName,
-        $person->getLink('ifpa'),
-        $person->getLink('photo'),
-        '<input type="button" id="'.$person->id.'_isMe">'
-      );
-    }
-    debug($json);
-  } else {
-   echo '{"aaData": []}';
+  }
+  switch ($type) {
+    case 'regSearch':
+      if ($persons && count($persons) > 0) {
+        $json = (object) array(
+          'sEcho' => $_REQUEST['sEcho'],
+          'iTotalRecords' => count($persons),
+          'iTotalDisplayRecords' => count($persons),
+        );
+        foreach ($persons as $person) {
+          $json->aaData[] = array(
+            $person->name,
+            $person->shortName,
+            $person->cityName,
+            $person->regionName,
+            $person->countryName,
+            $person->getLink('ifpa'),
+            $person->getLink('photo'),
+            '<input type="button" id="'.$person->id.'_isMe">'
+          );
+        }
+        echo json_encode($json);
+      } else 
+        echo '{"aaData": []}';
+      }
+    break;
   }
 
 ?>
