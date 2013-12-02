d537487924c170f99b839273bdf72c41c53248d4
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-02 05:27:56 +0000

    register¨'

--- Flipper/EPC/test.europinball.com/classes/person.class.php
+++ Flipper/EPC/test.europinball.com/classes/person.class.php
@@ -98,6 +98,21 @@
       $id = $player->save();
     }
 
+    public function getLink($type = 'object') {
+      switch ($type) {
+        case 'ifpa':
+          if ($this->ifpa_id) {
+            return '<a href="http://www.ifpapinball.com/player.php?player_id='.$this->ifpa_id.'" target="_new">'.(($this->ifpaRank && $this->ifpaRank != 0) ? $this->ifpaRank : 'Unranked').'</a>';
+          } else {
+            return 'Unranked';
+          }
+        break;
+        default:
+          parent::getLink($type);
+        break;
+      }
+    }
+
 /*
     public function __construct($data, $type = NULL, $search = NULL) {
       switch ($type) {
