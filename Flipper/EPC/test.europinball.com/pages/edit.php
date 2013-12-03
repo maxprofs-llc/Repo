769335d8a3a5788c1302f8442e5d697b7a81ca34
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-03 06:20:40 +0000

    edit

--- Flipper/EPC/test.europinball.com/pages/edit.php
+++ Flipper/EPC/test.europinball.com/pages/edit.php
@@ -8,7 +8,7 @@
   if ($page->reqLogin) {
     $page->addParagraph('Hej.');
   } else {
-    $page->addParagraph('You need to be logged in and registered as a participant to access this page. Please go to the <a href="'.config::$baseHref.'/reigstration/">registration page</a>.')
+    $page->addParagraph('You need to be logged in and registered as a participant to access this page. Please go to the <a href="'.config::$baseHref.'/reigstration/">registration page</a>.');
   }
   
   $page->submit();
