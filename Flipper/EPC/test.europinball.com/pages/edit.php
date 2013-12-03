48385e1806da42ad7ef1c8c1dfecd51d6dffab18
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-03 16:23:38 +0000

    edit

--- Flipper/EPC/test.europinball.com/pages/edit.php
+++ Flipper/EPC/test.europinball.com/pages/edit.php
@@ -7,22 +7,26 @@
   
   if ($page->reqLogin('You need to be logged in and registered as a participant to access this page. Please go to the <a href="'.config::$baseHref.'/reigstration/">registration page</a> or login here:')) {
     $person = $page->login->person;
-    $player = $person->getPlayer();
-    $page->startDiv('editDiv');
-      $page->addContent($player->getEdit());
-    $page->closeDiv();
-    $page->jeditable = TRUE;
-    $page->addScript('
-      $.editable.addInputType("autocomplete", {
-        element: $.editable.types.text.element,
-        plugin: function(settings, original) {
-          $("input", this).autocomplete(settings.autocomplete.data);
-        }
-      });
-      $(".editText").editable("'.config::$baseHref.'/setPlayerProp.php", {
-        cssclass: "inherit"
-      });
-    ');
+    if ($person) {
+      $page->startDiv('editDiv');
+        $page->addContent($player->getEdit());
+      $page->closeDiv();
+      $page->jeditable = TRUE;
+      $page->combobox = TRUE;
+      $page->addScript('
+        $.editable.addInputType("autocomplete", {
+          element: $.editable.types.text.element,
+          plugin: function(settings, original) {
+            $("input", this).autocomplete(settings.autocomplete.data);
+          }
+        });
+        $(".editText").editable("'.config::$baseHref.'/setPlayerProp.php", {
+          cssclass: "inherit"
+        });
+      ');
+    } else {
+      error('Could not find you in the database?', TRUE);
+    }
   }
   
   $page->submit();
