69c5091f183119a29f0a0415f7945ac1d5efa88f
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-04 11:29:09 +0000

    edit

--- Flipper/EPC/test.europinball.com/pages/edit.php
+++ Flipper/EPC/test.europinball.com/pages/edit.php
@@ -21,7 +21,7 @@
               if (data.new_id) {
                 $("#" + data.new_obj + "_id option:eq(" + data.new_id + ")").prop("selected", true);
                 $("#" + data.new_obj + "_id").next("input").val($("#" + data.new_obj + "_id").children(":selected").text());
-                $("#" + data.new_obj + "_id").next("input").change();
+                $("#" + data.new_obj + "_id").next("input").trigger("keydown");
               }
               alert($(this).data("previous"));
               alert($(this).attr("data-previous"));
