ca344b7503f97f6ed84d7c9f02e1cad0fd6fd45d
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-06 04:44:10 +0000

    edit¨

--- Flipper/EPC/test.europinball.com/pages/edit.php
+++ Flipper/EPC/test.europinball.com/pages/edit.php
@@ -17,10 +17,10 @@
         $(".combobox").change(function(){
           var el = this;
           var combobox = document.getElementById(el.id + "_combobox");
-          showTooltip(combobox, "Updating the database...", 30);
+          showTooltip(combobox, "Updating the database...", 45);
           $.post("'.config::$baseHref.'/ajax/setPlayerProp.php", {prop: el.id, value: $(el).val()})
           .done(function(data) {
-            showTooltip(combobox, data.reason, 30);
+            showTooltip(combobox, data.reason, 45);
             if (data.valid) {
               $(combobox).val($(el).children(":selected").text());
               if (data.parents) {
@@ -45,7 +45,7 @@
             }
           })
           .fail(function(jqHXR,status,error) {
-            showTooltip(combobox, "Fail: S: " + status + " E: " + error, 30); // Oh, no! Fail!
+            showTooltip(combobox, "Fail: S: " + status + " E: " + error, 45); // Oh, no! Fail!
           })
         })
         .combobox();
