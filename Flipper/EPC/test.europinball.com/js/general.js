dd6d33bc90fe6e41cb5963ce6500bcf55dc3cb65
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-06 06:29:38 +0000

    edit

--- Flipper/EPC/test.europinball.com/js/general.js
+++ Flipper/EPC/test.europinball.com/js/general.js
@@ -6,6 +6,7 @@ var tooltips = [];
 
 function showTooltip(el, text, offset) {
   alert(text);
+  alert(el.id);
   var offset = (offset) ? offset : 15;
   if (typeof tooltips[el.id] != "undefined") {
     clearTimeout(tooltips[el.id]);
