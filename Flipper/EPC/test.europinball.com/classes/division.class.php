8c1ae9f5bb33507dad8163962947c4a81c901ea7
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-07 05:57:21 +0000

    edit

--- Flipper/EPC/test.europinball.com/classes/division.class.php
+++ Flipper/EPC/test.europinball.com/classes/division.class.php
@@ -52,7 +52,7 @@
       'player' => array(
         'field' => 'tournamentDivision', 
         'delete' => true
-      ),
+      ), 
       'team' => array(
         'field' => 'tournamentDivision',
         'delete' => true
