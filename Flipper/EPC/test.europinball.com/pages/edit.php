474f80630f1628cc99af39549f540ff1d8763245
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-08 07:28:05 +0000

    edit'

--- Flipper/EPC/test.europinball.com/pages/edit.php
+++ Flipper/EPC/test.europinball.com/pages/edit.php
@@ -12,6 +12,7 @@
       $page->addContent($person->getPhotoEdit(NULL, 'right')); 
       $page->jeditable = TRUE;
       $page->combobox = TRUE;
+      $page->tooltipster = TRUE;
       $page->forms = TRUE;
       $page->addScript('
         $(".combobox").change(function(){
