da19db8771c0f2fc9b3a6d5426ca97e48c4d8a48
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-08 05:20:59 +0000

    edit'

--- Flipper/EPC/test.europinball.com/classes/base.class.php
+++ Flipper/EPC/test.europinball.com/classes/base.class.php
@@ -45,6 +45,8 @@
             } else if (isObj($data) && get_class($obj) == get_class($this)) {
               $this->_set($data);
             } else if (isObj($data) && isId($data->id)) {
+              debug(get_class($obj),'objclass');
+              debug(get_class($this),'thisclass');
               $dataClass = get_class($data);
               $dataTable = (property_exists($data, 'table')) ? $dataClass::$table : $dataClass;
               $objSearch[$dataTable.'_id'] = $data->id;
