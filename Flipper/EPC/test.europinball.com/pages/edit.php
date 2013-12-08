66eeeb0d833b0d434f7261a67e1244c0a15e71ab
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-08 04:54:05 +0000

    edit'

--- Flipper/EPC/test.europinball.com/pages/edit.php
+++ Flipper/EPC/test.europinball.com/pages/edit.php
@@ -7,7 +7,6 @@
   
   if ($page->reqLogin('You need to be logged in and registered as a participant to access this page. Please go to the <a href="'.config::$baseHref.'/reigstration/">registration page</a> or login here:')) {
     $person = person('login');
-    debug($person);
     if ($person) {
       $page->addContent($person->getEdit());
       $page->addContent($person->getPhotoEdit(NULL, 'right')); 
