0e3ce4637968d8d1280e727c72ce5657f8c7ae6b
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-08 10:39:23 +0000

    edit'

--- Flipper/EPC/test.europinball.com/classes/player.class.php
+++ Flipper/EPC/test.europinball.com/classes/player.class.php
@@ -138,7 +138,7 @@
         }
       }
       if ((isObj($data) && get_class($data) == 'person') || (is_string($search) && in_array($search, $divisions))) {
-        $search = division($search);
+        $search = division((($search !== NOSEARCH) ? $search : NULL));
         if (!$search || !isId($search->id)) {
           $this->failed = TRUE;
           return FALSE;
