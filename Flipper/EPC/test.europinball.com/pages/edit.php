9f369d38e18acff27f3d0bcc45dcb384cc33d49b
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-08 08:34:29 +0000

    edit'

--- Flipper/EPC/test.europinball.com/pages/edit.php
+++ Flipper/EPC/test.europinball.com/pages/edit.php
@@ -52,7 +52,6 @@
         });
         $(".custom-combobox-input").on("autocompleteclose", function(event, ui) {
           if ($("#" + this.id.replace("_combobox", "")).is("select") && $(this).val() == "" && $("#" + this.id.replace("_combobox", "")).val() != 0) {
-alert("twice");
             $("#" + this.id.replace("_combobox", "")).val(0);
             $("#" + this.id.replace("_combobox", "")).change();
           }
