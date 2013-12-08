e4e0a693b05c382e1ff30c6f20475e5881894a66
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-08 15:17:48 +0000

    edit'

--- Flipper/EPC/test.europinball.com/classes/base.class.php
+++ Flipper/EPC/test.europinball.com/classes/base.class.php
@@ -379,25 +379,6 @@
                 $("#'.$prefix.'thumb").on("click", function() {
                   $("#'.$prefix.'imageUpload").trigger("click");
                 });
-                $("#'.$prefix.'submitImg").tooltipster({
-                  theme: ".tooltipster-light",
-                  content: "Saving the image...",
-                  interactiveAutoClose: false,
-                  position: "right",
-                  offsetX: 38,
-                  timer: 3000
-                })
-                .click(function() {
-                  var el = this;
-                  $(el).tooltipster("show");
-                  $.post("'.config::$baseHref.'/ajax/imageUpload.php", {obj: "'.get_class($this).'", id: '.$this->id.', previewPath: "'.preg_replace('!'.config::$baseHref.'!', '', $this->getPhoto(FALSE, FALSE)).'"})
-                  .done(function(data) {
-                    $(el).tooltipster("update", data.reason).tooltipster("show");
-                  })
-                  .fail(function(jqHXR,status,error) {
-                    $(el).tooltipster("update", "Fail: S: " + status + " E: " + error).tooltipster("show");
-                  });
-                });
               }); 
             </script>
           </form>
