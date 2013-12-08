aadecad68145e0721d512ca2e82f648c6f7726c6
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-08 15:07:47 +0000

    edit'

--- Flipper/EPC/test.europinball.com/classes/base.class.php
+++ Flipper/EPC/test.europinball.com/classes/base.class.php
@@ -370,6 +370,7 @@
                 $("#'.$prefix.'imageUpload").on("change", function() {
                   $("#'.$prefix.'preview").html("");
                   $("#'.$prefix.'imageLoader").html("<img src=\"'.config::$baseHref.'/images/loader.gif\" alt=\"Uploading....\"/>");
+                  $("#'.$prefix.'submitImg").button("option", "disabled", false);
                   $("#'.$prefix.'imageForm").ajaxForm({
                     target: "#'.$prefix.'preview"
                   }).submit();
