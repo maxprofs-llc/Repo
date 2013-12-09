ad4e7a36034c52b8995ba3ee91ff07a3c05c3816
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-09 11:37:12 +0000

    edit'''

--- Flipper/EPC/test.europinball.com/functions/init.php
+++ Flipper/EPC/test.europinball.com/functions/init.php
@@ -2,10 +2,15 @@
 
   require_once('classes.php');
   require_once('general.php');
+  
   config::$login = new auth();
-   
+  
   if (isId($_REQUEST['tournament']) || isId($_REQUEST['t'])) {
     config::$currentTournament =(isId($_REQUEST['tournament'])) ? $_REQUEST['tournament'] : $_REQUEST['t'];
   }
-
+  
+  if ($_REQUEST['action']) {
+    config::$login->action($_REQUEST['action']);
+  }
+   
 ?>
