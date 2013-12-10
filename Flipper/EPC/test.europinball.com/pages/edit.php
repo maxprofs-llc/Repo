9b4a1f0b40af2d1c075d1121fee994e7a42e0882
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-10 07:52:49 +0000

    edit

--- Flipper/EPC/test.europinball.com/pages/edit.php
+++ Flipper/EPC/test.europinball.com/pages/edit.php
@@ -175,7 +175,7 @@
                 $page->startDiv($division->type.'CostDiv');
                   $cost = $person->getCost($division);
                   $costs += $cost;
-                  $page->addInput(1, $division->type.'Num', $division->type.'Num', 'text', 'cost', camelCaseToSpace($division->type));
+                  $page->addInput(1, $division->type.'Num', $division->type.'Num', 'text', 'cost', camelCaseToSpace($division->type), TRUE);
                   $page->addSpan($cost, $division->type.'Cost', 'currency');
                   $page->addInput($cost, $division->type.'Each', $division->type.'Each', 'hidden', 'each');
                 $page->closeDiv();
