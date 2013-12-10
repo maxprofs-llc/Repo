51dcb0837e412342f650f3084b4c55236c36a3f5
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-10 16:19:27 +0000

    edit

--- Flipper/EPC/test.europinball.com/pages/edit.php
+++ Flipper/EPC/test.europinball.com/pages/edit.php
@@ -168,7 +168,7 @@
               } catch(e) {
                 var curVal = 0;
               };
-              curVal = parseInt(curVal);
+              curVal = (parseInt(curVal)) ? parseInt(curVal) : 0;
               alert(curVal);
               $("#currency").val(curVal)
               .combobox()
