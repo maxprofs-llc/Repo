b2d1da7afc0e0d3cca4c29d17ce897659326ce7c
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-10 03:33:57 +0000

    edit

--- Flipper/EPC/test.europinball.com/pages/edit.php
+++ Flipper/EPC/test.europinball.com/pages/edit.php
@@ -158,7 +158,7 @@
             $page->addH2('Payment options');
             $divisions = divisions('active');
             foreach ($divisions as $division) {
-              if (property_exists('config', $division->type.'Cost') && config::${$division->type.'Cost'}) {
+              if (property_exists('config', $division->type.'Cost')) {
                 $page->startDiv();
                   $page->addInput($person->getCost($division), config::${$division->type.'Cost'});
                 $page->closeDiv();
