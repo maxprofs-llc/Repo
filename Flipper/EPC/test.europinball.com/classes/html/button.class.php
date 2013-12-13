3ff5189d25e004d9b6dd8f9237c746b98eee1da0
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-13 13:41:10 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/button.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/button.class.php
@@ -2,21 +2,17 @@
 
   class button extends input {
     
-    protected $form;
+    protected $script;
     
-    public function __construct($value = 'submit', $name = NULL, $form = TRUE, array $params = NULL) {
+    public function __construct($value = 'submit', $name = NULL, array $params = NULL) {
       $params['name'] = ($name) ? $name : $value;
-      if ($form === TRUE) {
-        $this->form = new form($name.'Form');
-      } else if (is($form)) {
-        $this->form = (isHtml($form)) ? $form : new form($form);
-      }
+      $params['id'] = ($params['id']) ? $params['id'] : $name;
       parent::__construct($name, $value, 'button', FALSE, $params);
       $this->inline = true;
     }
 //    input public function __construct($name = NULL, $value = NULL, $type = 'text', $label = TRUE, array $params = NULL) {
 //    html public function __construct($element = 'span', $contents = NULL, array $params = NULL, $id = NULL, $class = NULL, array $css = NULL, $indents = 0) {
-    
+
   }
   
 ?>
