3e2c0ff65f0eab82c1801c13c588bbdb6a0bd6d8
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-13 13:42:58 +0000

    '

--- Flipper/EPC/test.europinball.com/pages/test.php
+++ Flipper/EPC/test.europinball.com/pages/test.php
@@ -32,7 +32,7 @@
   $el->addElement('div', 'Innerdiv4!', array('class' => 'nlah', 'src' => 'asas'), 'innerId4', 'innerKlass', array('color' => 'black'));
   $div = $el->addDiv('newDiv', 'newDivKlass', array('title' => 'newDivTitle'));
   $h = new hidden('action', 'login');
-  $b = new button('Click me!');
+  $b = new clickButton('Click me!');
   $el->addContent($h);
   $el->addContent($b);
   $div->addContent($img);
