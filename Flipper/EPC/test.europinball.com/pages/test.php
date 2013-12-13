f12015e9644d61e10bd424f98cb11702840c22a0
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-13 21:31:30 +0000

    '

--- Flipper/EPC/test.europinball.com/pages/test.php
+++ Flipper/EPC/test.europinball.com/pages/test.php
@@ -43,7 +43,10 @@
   $span2->entities = TRUE;
   $js = new scriptCode('var scriptcode = true;');
   $div4 = $div3->addDiv('4');
-  $tooltip = $div4->addTooltip('Hej!', FALSE);
+  $tooltip = $div4->addTooltip('                $("#Clickme").click(function() {
+                  $("#ClickmeForm").submit();
+                });
+'');
 //  debug($tooltip);
   $img->src = 'a_src';
   $div4->addElement($js);
