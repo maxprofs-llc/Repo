65d9402573b8de02c0259d78cda7050c4af72c4c
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-13 10:46:49 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/jquery.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/jquery.class.php
@@ -19,7 +19,7 @@
         $options->indent_level = static::$indents;
         $options->max_preserve_newlines = 1;
         $jsbeautifier = new JSBeautifier();
-        return static::$indenter.ltrim($jsbeautifier->beautify((($this->settings['onReady']) ? static::$indenter."$(document).ready(function() {\n" : '').parent::getContent($index, $string).(($this->settings['onReady']) ? "\n});" : ''), $options));
+        return ltrim($jsbeautifier->beautify((($this->settings['onReady']) ? static::$indenter."$(document).ready(function() {\n" : '').parent::getContent($index, $string).(($this->settings['onReady']) ? "\n});" : ''), $options));
       }
     }
 
