074e1ad7c7716802aad6b6dc6e76bed228d5f7ee
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-13 17:33:48 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/tooltip.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/tooltip.class.php
@@ -3,9 +3,7 @@
   class tooltip extends jquery {
     
     public function __construct($selector = NULL, $contents = NULL, $new = TRUE, $indents = 0) {
-      $object = 'tooltipster';
       if ($new) {
-      $function = TRUE;
       $settings = array(
           'theme' => '.tooltipster-light',
           'trigger' => 'custom',
@@ -13,11 +11,10 @@
           'timer' => 3000
         );
       } else {
-        $function = FALSE;
         $command = array('update', 'show');
         $contents = array($contents, FALSE);
       }
-      parent::__construct($selector, $this->jquery['tooltipster'], $this->jquery['function'], $this->jquery['command'], $contents, $settings, $indents);
+      parent::__construct($selector, 'tooltipster', $new, $command, $contents, $settings, $indents);
     }
 //    jquery public function __construct($selector = NULL, $object = NULL, $function = NULL, $comamnd = NULL, $contents = NULL, array $settings, $indents = 0) {
 //    scriptCode public function __construct($source = NULL, array $params = NULL, $indents = 0) {
@@ -35,7 +32,7 @@
           }
           $code .= 'content: "'.parent::getContent($index, $string).'"';
           return $code."\n";
-        } else {
+        } else { 
           return parent::getContent($index, $string);
         }
       }
