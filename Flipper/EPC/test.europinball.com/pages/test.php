14fe1434a5f767bcbd6ad8fa35b6205e481eb4a8
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-13 13:16:21 +0000

    '

--- Flipper/EPC/test.europinball.com/pages/test.php
+++ Flipper/EPC/test.europinball.com/pages/test.php
@@ -31,6 +31,8 @@
   $el2->addElement('div', 'Innerdiv3!', array('class' => 'nlah', 'src' => 'asas'), 'innerId3', 'innerKlass', array('color' => 'black'));
   $el->addElement('div', 'Innerdiv4!', array('class' => 'nlah', 'src' => 'asas'), 'innerId4', 'innerKlass', array('color' => 'black'));
   $div = $el->addDiv('newDiv', 'newDivKlass', array('title' => 'newDivTitle'));
+  $h = new hidden('action' => 'login');
+  el->addContent($h);
   $div->addContent($img);
   $div1 = $el->addDiv('1');
   $div2 = $div1->addDiv('2');
