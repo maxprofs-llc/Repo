016bb64b3cd15785ecb324503c0f535f0901fa5c
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-13 13:19:15 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/input.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/input.class.php
@@ -44,11 +44,10 @@
         case 'insideLabel':
           $this->settings['insideLabel'] = ($value);
           if ($value) {
-            if ($this->label) {
+            if (!$this->label) {
               $this->label = new label(ucfirst($name), $name.'Label');
             }
             $this->label->addContent($this, $this, $this->settings['beforeLabel']);
-            }
           } else {
             $this->label->delContent($this);
           }
