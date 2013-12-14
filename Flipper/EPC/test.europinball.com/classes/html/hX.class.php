299eb17510124bd8562cdd35affbf69284461686
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-14 14:49:38 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/hX.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/hX.class.php
@@ -2,11 +2,10 @@
 
   class hX extends html {
     
-    protected $hLevel = 1;
-    
     public function __construct($hLevel = 1, $contents = NULL, array $params = NULL) {
-      $hLevel = ($hLevel && is_int($hLevel) && $hLevel < 5) ? $hLevel : 1;
-      parent::__construct('h'.$hLevel, $contents, $params);
+      $this->settings['hLevel'] = ($hLevel && is_int($hLevel) && $hLevel < 5) ? $hLevel : 1;
+      $this->inline = TRUE;
+      parent::__construct('h'.$this->settings['hLevel'], $contents, $params);
     }
 //    html public function __construct($element = 'span', $contents = NULL, array $params = NULL, $id = NULL, $class = NULL, array $css = NULL, $indents = 0) {
     
--- Flipper/EPC/test.europinball.com/classes/html/li.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/li.class.php
@@ -3,8 +3,8 @@
   class li extends html {
     
     public function __construct($contents = NULL, $id = NULL, $class = NULL, array $params = NULL) {
+      $this->inlineBlock = TRUE;
       parent::__construct('li', $contents, $params, $id, $class, $css);
-      $this->block = true;
     }
 //    html public function __construct($element = 'span', $contents = NULL, array $params = NULL, $id = NULL, $class = NULL, array $css = NULL, $indents = 0) {
     
