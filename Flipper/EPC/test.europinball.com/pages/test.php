af91aa8ee651d0004274126b91992710b8e14e26
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-14 14:18:19 +0000

    '

--- Flipper/EPC/test.europinball.com/pages/test.php
+++ Flipper/EPC/test.europinball.com/pages/test.php
@@ -78,7 +78,6 @@ $div3->addLabel('Some content', 'forName', 'ID', 'klasses', array('sampleparam' 
 $sel = $div3->addSelect('Namnet', NULL, NULL, TRUE, array('sampleparam' => 'huff'));
 $opt = $sel->addOption('Välj mig!', 'vald', 'vald', array('sampleparam' => 'huff'));
 $sel2 = $div3->addSelect('Namnet', $opt, $opt, TRUE, array('sampleparam' => 'huff'));
-debug(NULL, 'die', TRUE);
 $opt2 = $sel->addOption('Välj inte mig!', 'ejvald', FALSE, array('sampleparam' => 'huff'));
 $opts = array($opt, $opt2);
 $div3->addSelect('Namnet', $opts, $opt2, FALSE, array('sampleparam' => 'huff'));
