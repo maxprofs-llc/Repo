915e09f753100041fbc75637d4bda944dabdf832
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-14 15:01:28 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/hX.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/hX.class.php
@@ -4,7 +4,7 @@
     
     public function __construct($hLevel = 1, $contents = NULL, array $params = NULL) {
       $this->settings['hLevel'] = ($hLevel && is_int($hLevel) && $hLevel < 5) ? $hLevel : 1;
-      $this->inline = TRUE;
+      $this->inlineBlock = TRUE;
       parent::__construct('h'.$this->settings['hLevel'], $contents, $params);
     }
 //    html public function __construct($element = 'span', $contents = NULL, array $params = NULL, $id = NULL, $class = NULL, array $css = NULL, $indents = 0) {
