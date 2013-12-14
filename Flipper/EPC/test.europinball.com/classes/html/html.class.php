4a0b5348cce3a2e8f5b833475dfee9e442d966c8
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-14 14:51:36 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/html.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/html.class.php
@@ -813,7 +813,7 @@
         $html .= $this->getFooter();
       }
       $html .= ($this->contentCrlf && $html && substr($close, 0, strlen($this->contentCrlf)) != $this->contentCrlf && substr(trim($html, static::$indenter), strlen($this->contentCrlf) * -1) != $this->contentCrlf) ? $this->contentCrlf : '';
-      $open .= ($this->contentCrlf && $html && substr($open, strlen($this->contentCrlf) * -1) != $this->contentCrlf && substr(trim($html, static::$indenter), 0, strlen($this->contentCrlf)) != $this->contentCrlf) ? $this->contentCrlf.$indent.static::$indenter : '';
+      $open .= ($this->contentCrlf && substr($open, strlen($this->contentCrlf) * -1) != $this->contentCrlf && substr(trim($html, static::$indenter), 0, strlen($this->contentCrlf)) != $this->contentCrlf) ? $this->contentCrlf.$indent.static::$indenter : '';
       return $open.$html.$close;
     }
 
