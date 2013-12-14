898eeafd504516f574e21d6aeaea0a5e989ade59
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-14 05:50:02 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/paragraph.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/paragraph.class.php
@@ -3,7 +3,7 @@
   class paragraph extends html {
     
     public function __construct($contents = NULL, $id = NULL, $class = NULL, array $params = NULL) {
-      parent::__construct('p', $contents, $params, $id, $class, $css);
+      parent::__construct('p', $contents, $params, $id, $class);
     }
 //    html public function __construct($element = 'span', $contents = NULL, array $params = NULL, $id = NULL, $class = NULL, array $css = NULL, $indents = 0) {
     
