31e0beb47bbd66f4da226b5288e8b1a1f8951fa8
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-14 16:22:34 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/html.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/html.class.php
@@ -305,7 +305,7 @@
     }
 
     protected function get($section = 'contents', $index = NULL, $string = TRUE) {
-      if (!$this->selfClose) {
+      if (!$this->selfClose && !$this->contentParam) {
         if(is($index)) {
           $html .= ($string) ? static::contentToHtml($this->$section[$index], $this->settings['escape'], $this->settings['entities']) : $this->$section[$index];
         } else {
