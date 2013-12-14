7f504baa47542850cc101fc84218b3aa90b6a42d
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-14 14:21:06 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/html.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/html.class.php
@@ -781,7 +781,7 @@
     }
     
     public function getHtml() {
-     $this->debug(get_class($this), 'CLASS', NULL, 50);
+     $this->debug(get_class($this), 'CLASS', NULL, 150);
      if ($this->crlf) {
         $indents = (is($this->localIndents)) ? $this->localIndents : static::$indents;
         while ($i < $indents) {
