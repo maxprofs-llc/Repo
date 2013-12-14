9275c83d8f276b5843e882bf9ffed1f6ad67916b
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-14 09:49:10 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/clickButton.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/clickButton.class.php
@@ -5,6 +5,7 @@
     public function __construct($value = 'submit', $name = NULL, $url = NULL, $form = TRUE, $script = TRUE, array $params = NULL) {
       $params['name'] = ($name) ? $name : $value;
       $params['id'] = ($params['id']) ? $params['id'] : preg_replace('/[^a-zA-Z0-9]/', '', $params['name']);
+      $this->id = $params['id'];
       $this->form($form, $url);
       $this->script($script);
       $this->inline = true;
