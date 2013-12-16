f648643a03dbb3637227e145cf619f8ff9bd81d4
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-16 05:36:44 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/group.class.php
+++ Flipper/EPC/test.europinball.com/classes/group.class.php
@@ -20,7 +20,7 @@
       } else if (is_array($data) || isGroup($data)) {
         $objs = [];
         foreach ($data as $obj) {
-          debug(get_class($data));
+          debug(get_class($obj));
           if ($obj->id) {
             if (get_class($data) == static::$objClass) {
               $objs[] = $data;
