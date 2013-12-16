1b53641aa35a78f2dad4d47ca916b71066f4bb8c
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-16 11:18:02 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/html/combobox.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/combobox.class.php
@@ -5,8 +5,9 @@
     public function __construct($selector = NULL, $indents = 0) {
       $this->settings['jsReq'][] = 'jquery.combobox';
       $this->settings['cssReq'][] = 'jquery.combobox';
-      parent::__construct($selector, 'combobox', 'command', NULL, NULL, $settings, $indents);
+      parent::__construct($selector, 'combobox', 'command', NULL, NULL, $indents);
     }
+//    jqueryui public function __construct($selector = NULL, $tool = NULL, $jqtype = NULL, $contents = NULL, array $props = NULL, $indents = 0) {
 //    jquery public function __construct($selector = NULL, $tool = NULL, $jqtype = NULL, $contents = NULL, array $props = NULL, $indents = 0) {
 //    scriptCode public function __construct($source = NULL, array $params = NULL, $indents = 0) {
 //    html public function __construct($element = 'span', $contents = NULL, array $params = NULL, $id = NULL, $class = NULL, array $css = NULL, $indents = 0) {
