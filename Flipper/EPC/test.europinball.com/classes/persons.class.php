9cb0a0652514ee7f109a5b70cca68f2f1ff21353
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-16 05:05:21 +0000

    '

--- Flipper/EPC/test.europinball.com/classes/persons.class.php
+++ Flipper/EPC/test.europinball.com/classes/persons.class.php
@@ -4,6 +4,18 @@
     
     public static $objClass = 'person';
     
+    public function __construct($data = NULL, $search = config::NOSEARCH, $depth = NULL) {
+      if (isPlayers($data) || isDivision($data) || isTournament($data)) {
+        parent::__construct(NULL, config::NOSEARCH, $depth);
+        $players = players($data);
+        foreach ($players as $player) {
+          $this[] = $player->person;
+        }
+      } else {
+        parent::__construct($data, $search, $depth);
+      }
+    }
+
   }
 
 ?>
