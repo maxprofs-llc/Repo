24845c98c5d55dab4bc5be647aec8614c47f2617
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-16 12:34:12 +0000

    '

--- Flipper/EPC/test.europinball.com/pages/test.php
+++ Flipper/EPC/test.europinball.com/pages/test.php
@@ -11,7 +11,8 @@
   $select->addCombobox();
   $div->addContent($select);
   $paidDiv = $div->addDiv();
-  $paidDiv->addLabel('Paid:')->addCss('margin-top', '15px');
+  $label = $paidDiv->addLabel('Paid:')
+  $label->addCss('margin-top', '15px');
   $paidSpan = $paidDiv->addMoneySpan(0, 'paid', config::$currencies[config::$defaultCurrency]['format']);
   $shouldDiv = $div->addDiv();
   $shouldDiv->addLabel('Should pay:');
