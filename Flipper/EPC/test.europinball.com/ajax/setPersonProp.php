60dfc6c6fe6930b264b05546bcadb7be0cb59502
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-19 09:57:39 +0000

    sort

--- Flipper/EPC/test.europinball.com/ajax/setPersonProp.php
+++ Flipper/EPC/test.europinball.com/ajax/setPersonProp.php
@@ -14,7 +14,7 @@
   if ($value || $value == 0) {
     if ($prop) {
       $loginPerson = person('login');
-      debug($loginPersson);
+      debug($loginPerson);
       if ($person_id) {
         $person = person($person_id);
       } else {
