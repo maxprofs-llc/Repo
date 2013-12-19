a7d3713b54ae26bf4533dcd3784358673ac48555
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-19 08:51:26 +0000

    sort

--- Flipper/EPC/test.europinball.com/pages/test.php
+++ Flipper/EPC/test.europinball.com/pages/test.php
@@ -14,6 +14,7 @@
   $paidDiv = $div->addDiv('paidDiv');
   $paidDiv->addLabel('Paid:')->addCss('margin-top', '15px');
   $paidSpan = $paidDiv->addMoneySpan(0, 'paid', config::$currencies[config::$defaultCurrency]['format']);
+  debug($paidSpan);
   $shouldDiv = $div->addDiv('shouldDiv');
   $shouldDiv->addLabel('Should pay:');
   $paySpan = $shouldDiv->addMoneySpan(0, 'pay', config::$currencies[config::$defaultCurrency]['format']);
