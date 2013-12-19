6df217cf68e79cc1812b01e8477fdf715645217d
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-19 15:28:57 +0000

    sort'

--- Flipper/EPC/test.europinball.com/classes/html/dialog.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/dialog.class.php
@@ -8,6 +8,7 @@
       $props['width'] = ($props['width']) ? $props['width'] : "auto";
       $props['height'] = ($props['height']) ? $props['height'] : "auto";
       $props['dialogClass'] = ($props['dialogClass']) ? $props['dialogClass'] : "noTitleBar transparent";
+      $props['resizable'] = ($props['resizable']) ? $props['resizable'] : FALSE;
       parent::__construct($selector, 'dialog', 'object', NULL, $props, $indents);
     }
 //    jqueryui public function __construct($selector = NULL, $tool = NULL, $jqtype = NULL, $contents = NULL, array $props = NULL, $indents = 0) {
