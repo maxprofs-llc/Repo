730bd530275cd999d92ef6505231e7a8f0a47fce
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-19 07:37:36 +0000

    sort

--- Flipper/EPC/test.europinball.com/js/contrib/jquery.combobox.js
+++ Flipper/EPC/test.europinball.com/js/contrib/jquery.combobox.js
@@ -119,7 +119,7 @@
 //          .tooltip( "open" );
         this.element.val( "" );
         this._delay(function() {
-          this.input.tooltip( "close" ).attr( "title", "" );
+//          this.input.tooltip( "close" ).attr( "title", "" );
         }, 60000 );
         this.input.data( "ui-autocomplete" ).term = "";
       },
