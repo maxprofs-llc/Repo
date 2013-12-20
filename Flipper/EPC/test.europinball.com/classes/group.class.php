0fe7ae9d066669c17700da63cb815954340feeaa
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-20 11:30:43 +0000

    admin

--- Flipper/EPC/test.europinball.com/classes/group.class.php
+++ Flipper/EPC/test.europinball.com/classes/group.class.php
@@ -47,8 +47,8 @@ debug('GR2');
           if (isObj($search)) {
             $props = array(
               (property_exists($data, 'table')) ? get_class_vars(get_class($data))['table'].'_id' : get_class($data).'_id',
-              (property_exists($search, 'table')) ? get_class_vars(get_class($search))['table'].'_id' : get_class($search).'_id';
-            }
+              (property_exists($search, 'table')) ? get_class_vars(get_class($search))['table'].'_id' : get_class($search).'_id'
+            );
             $vals = array($data->id, $search->id);
             $objs = $this->db->getObjectsByProps(static::$objClass, $props, $vals);
           } else {
