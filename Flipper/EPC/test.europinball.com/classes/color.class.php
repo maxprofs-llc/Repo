a415a44d8ecba894e4ce4d8215e9abb4ee2bbf5b
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-21 03:41:32 +0000

    tshirt'

--- Flipper/EPC/test.europinball.com/classes/color.class.php
+++ Flipper/EPC/test.europinball.com/classes/color.class.php
@@ -12,7 +12,7 @@
         o.r as r,
         o.g as g,
         o.b as b,
-        concat(HEX(o.r), HEX(o.g), HEX(o.b)) as rgb,
+        concat(format(LPAD(HEX(o.r), 2, 0), LPAD(HEX(o.g), 2, 0), LPAD(HEX(o.b), 2, 0)) as rgb,
         o.c as c,
         o.m as m,
         o.y as y,
