a63ca0f2f88b9ec9bfa9f8c2b6a5019c45418994
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-23 06:03:05 +0000

    admin

--- Flipper/EPC/test.europinball.com/classes/html/label.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/label.class.php
@@ -7,7 +7,7 @@
         $params['for'] = $for;
       } 
       $class = (!is($class) && $class !== FALSE) ? 'label' : $class;
-      $id = (is($id)) ? $id : (($for) ? $for.'Label' : static::newId(NULL, 'Label');
+      $id = (is($id)) ? $id : (($for) ? $for.'Label' : static::newId(NULL, 'Label')); 
       parent::__construct('label', $contents, $params, $id, $class, $css);
       $this->inline = true;
     }
