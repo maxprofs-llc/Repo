460336d7983dd57807ae8fe6e4c20f8d3d98e041
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-23 10:35:25 +0000

    admin

--- Flipper/EPC/test.europinball.com/pages/edit.php
+++ Flipper/EPC/test.europinball.com/pages/edit.php
@@ -163,7 +163,7 @@
           */
           $tshirtDiv = new div('tshirts');
           $tshirtDiv->addContent($person->getEdit('tshirts', 'T-shirt orders'));
-          $tshitDiv->addContent('&nbsp;');
+          $tshirtDiv->addContent('&nbsp;');
           $page->addContent($tshirtDiv);
         }
         if (in_array('volunteer', config::$editSections)) {
@@ -171,7 +171,7 @@
             $volDiv->indents = 3;
             $paragraph = $volDiv->addParagraph('Volunteer registration will open in short.');
           $page->addContent($volDiv->getHtml());
-        }
+        } 
         if (in_array('payment', config::$editSections)) {
           $page->startDiv('payment');
             $page->addH2('Payment options');
