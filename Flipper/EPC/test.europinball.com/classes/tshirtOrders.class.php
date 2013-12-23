a9bd5df8afc76ba074c567fd6cbdb4a02e7cc105
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-23 08:12:03 +0000

    admin

--- Flipper/EPC/test.europinball.com/classes/tshirtOrders.class.php
+++ Flipper/EPC/test.europinball.com/classes/tshirtOrders.class.php
@@ -4,6 +4,12 @@
     
     public static $objClass = 'tshirtOrder';
     
+    public static $order = array(
+      'color_id',
+      'size_id'
+    );
+
+    
     public function __construct($data = NULL, $prop = NULL, $cond = 'and') {
       parent::__construct($data, $prop, $cond);
     }
