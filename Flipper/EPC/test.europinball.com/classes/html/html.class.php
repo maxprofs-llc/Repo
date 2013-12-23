bda2025d94ef421544dfd221e6c1f8d0c6dda441
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-23 07:00:20 +0000

    admin

--- Flipper/EPC/test.europinball.com/classes/html/html.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/html.class.php
@@ -44,6 +44,7 @@
         html::$ids[] = $params['id'];
       }
       $params['title'] = (is($params['title'])) ? $params['title'] : preg_replace('/'.ucfirst($this->element).'$/', '', ucfirst($params['id']));
+      $class = mergeToArray($class, $params['class']);
       if (get_class($this) == 'html') {
         $this->selfClose = (in_array($this->element, array('input', 'img', 'hr', 'br', 'meta', 'link'))) ? TRUE : FALSE;
         $this->crlf = (in_array($this->element, array('a', 'img', 'span', 'label'))) ? NULL : "\n";
