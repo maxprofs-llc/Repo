fe8cc59a3e1aec1ed3acc62c68a99c3598447239
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-24 21:15:11 +0000

    tshirt

--- Flipper/EPC/test.europinball.com/ajax/tshirtOrder.php
+++ Flipper/EPC/test.europinball.com/ajax/tshirtOrder.php
@@ -33,7 +33,6 @@
               } else if ($number == 0) {
                 $tshirtOrder->delete();
                 $tshirtOrder = tshirtOrder($tshirtOrder_id);
-                debug($tshirtOrder, 'order');
                 if (!$tshirtOrder) {
                   $json = success('Removed T-shirt order ID '.$tshirtOrder_id, array('newId' => 0));
                 } else {
