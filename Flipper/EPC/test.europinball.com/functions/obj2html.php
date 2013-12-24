0f275944f6595d54b179dd733ad75a65ac8857f9
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-24 22:55:43 +0000

    tshirt

--- Flipper/EPC/test.europinball.com/functions/obj2html.php
+++ Flipper/EPC/test.europinball.com/functions/obj2html.php
@@ -6,7 +6,7 @@
     $currencyProps = config::$currencies;
     $prefix = ($prefix) ? $prefix : html::newId();
     $select = new combobox($prefix.'Currency', $currencies, $defaultCurrency, 'Currency');
-    return select;
+    return $select;
   }
 
 ?>
