963f21ceccb8f0ec1e0f22ab0d3eed057d6aaff7
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-26 01:40:32 +0000

    tshirt

--- Flipper/EPC/test.europinball.com/classes/html/label.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/label.class.php
@@ -6,7 +6,7 @@
       if (is($for)) {
         $params['for'] = $for;
       } 
-      $class = (!is($class) && $class !== FALSE) ? 'label' : $class;
+      $class = (!is($class) && $class !== FALSE) ? 'label' : $class.' label';
       $id = (is($id)) ? $id : (($for) ? $for.'Label' : static::newId(NULL, 'Label')); 
       parent::__construct('label', $contents, $params, $id, $class, $css);
       $this->inline = true;
