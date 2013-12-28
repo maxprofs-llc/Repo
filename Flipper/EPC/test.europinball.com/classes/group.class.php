3414b34b122e3fb7f96a6478f5628813ae253755
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-28 02:53:14 +0000

    team

--- Flipper/EPC/test.europinball.com/classes/group.class.php
+++ Flipper/EPC/test.europinball.com/classes/group.class.php
@@ -74,7 +74,6 @@
             and pl.id is not null
           group by o.id
         ';
-        debug($data);
         $objs = $this->db->getObjectsByWhere(static::$objClass, $data);
       } else {
         if (isAssoc($data)) {
