dda57cd790474acac025034931a3b69a06cc32a3
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-29 00:32:47 +0000

    'objects'

--- Flipper/EPC/test.europinball.com/classes/game.class.php
+++ Flipper/EPC/test.europinball.com/classes/game.class.php
@@ -3,47 +3,28 @@
   class game extends base {
         
     public static $instances;
-    public static $arrClass = 'machines';
+    public static $arrClass = 'games';
 
     public static $select = '
       select 
-        o.id as id,
-        g.íd as game_id,
-        g.name as name,
-        g.name as fullName,
-        g.acronym as acronym,
-        g.acronym as shortName,
-        g.manufacturer_id as manufacturer_id,
-        g.game_ipdb_id as ipdb,
-        g.game_link_rulesheet as rules,
-        g.game_year_released as year,
-        o.tournamentDivision_id as tournamentDivision_id,
-        o.tournamentEdition_id as tournamentEdition_id,
-        o.gameType as type,
-        o.balls as balls,
-        o.extraBalls as extraBalls,
-        o.onePlayerAllowed as onePlayerAllowed,
-        o.owner_id as owner_id,
-        o.paid as paid,
-        o.comment as comment
-      from machine o
-      left join game g
-        on o.game_id = g.id
+        o.íd as id,
+        o.name as name,
+        o.name as fullName,
+        o.acronym as acronym,
+        o.acronym as shortName,
+        o.manufacturer_id as manufacturer_id,
+        o.game_ipdb_id as ipdb,
+        o.game_link_rulesheet as rules,
+        o.game_year_released as year
+      from game o
     ';
     
     public static $parents = array(
-      'game' => 'game',
-      'manufacturer' => 'manufacturer',
-      'tournamentDivision' => 'division',
-      'tournamentEdition' => 'tournament',
-      'owner' => 'owner'
+      'manufacturer' => 'manufacturer'
     );
 
     public static $children = array(
-      'gamachineme' => 'game',
-      'matchScore' => 'game',
-      'set' => 'game',
-      'score' => 'game'
+      'machine' => 'game'
     );
 
   }
