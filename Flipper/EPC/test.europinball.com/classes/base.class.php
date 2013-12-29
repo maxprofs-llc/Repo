bdad32bcad91adef74b4f28337e0c18274afb1d8
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-29 02:03:00 +0000

    'objects'

--- Flipper/EPC/test.europinball.com/classes/base.class.php
+++ Flipper/EPC/test.europinball.com/classes/base.class.php
@@ -217,7 +217,6 @@
       $left = $info->addDiv($this->id.'_'.get_class($this).'_InfoDivLeft', 'left');
       if (static::$infoProps) {
         foreach (static::$infoProps as $label => $prop) {
-          debug($prop);
           $html = FALSE;
           if (isObj($this->$prop)) {
             $link = $this->$prop->getLink('object', FALSE);
