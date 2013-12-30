bc177dfbacb962c9a8acc7bb55ed4f1137f8d1f6
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-30 01:57:37 +0000

    'games'

--- Flipper/EPC/test.europinball.com/pages/registered.php
+++ Flipper/EPC/test.europinball.com/pages/registered.php
@@ -61,82 +61,84 @@
       $page->closeUl();
       foreach ($divisions as $division) {
         $rows = array();
-        if (count($objs[$division->id]) > 0) {
+        if (count($objs[$division->id]) > 0 || config::$showEmptyDivisions) {
           $page->startDiv($division->shortName.ucfirst($type));
-          if ($type == 'players') {
-            if ($division->team) {
-              if ($division->national) {
-                $headers = array('Name', 'Tag', 'Country', 'Members', 'Picture');
-                $aoColumnDefs = '{"sClass": "icon", "aTargets": [ 4 ] }';
+          if (count($objs[$division->id]) > 0) {
+            if ($type == 'players') {
+              if ($division->team) {
+                if ($division->national) {
+                  $headers = array('Name', 'Tag', 'Country', 'Members', 'Picture');
+                  $aoColumnDefs = '{"sClass": "icon", "aTargets": [ 4 ] }';
+                } else {
+                  $headers = array('Name', 'Tag', 'Members', 'Picture');
+                  $aoColumnDefs = '{"sClass": "icon", "aTargets": [ 3 ] }';
+                }
               } else {
-                $headers = array('Name', 'Tag', 'Members', 'Picture');
-                $aoColumnDefs = '{"sClass": "icon", "aTargets": [ 3 ] }';
+                $headers = array('Name', 'Tag', 'City', 'Region', 'Country sort', 'Country', 'IFPA Rank', 'IFPA', 'Photo', 'Waiting', 'Paid');
+                $aoColumnDefs = '
+                  { "aDataSort": [ 6 ], "aTargets": [ 7 ] },
+                  { "bVisible": false, "aTargets": [ 6 ] },
+                  { "aDataSort": [ 4 ], "aTargets": [ 5 ] },
+                  { "bVisible": false, "aTargets": [ 4 ] },
+                  {"sClass": "icon", "aTargets": [ 5 ] },
+                  {"sClass": "icon", "aTargets": [ 8 ] }
+                ';
               }
-            } else {
-              $headers = array('Name', 'Tag', 'City', 'Region', 'Country sort', 'Country', 'IFPA Rank', 'IFPA', 'Photo', 'Waiting', 'Paid');
-              $aoColumnDefs = '
-                { "aDataSort": [ 6 ], "aTargets": [ 7 ] },
-                { "bVisible": false, "aTargets": [ 6 ] },
-                { "aDataSort": [ 4 ], "aTargets": [ 5 ] },
-                { "bVisible": false, "aTargets": [ 4 ] },
-                {"sClass": "icon", "aTargets": [ 5 ] },
-                {"sClass": "icon", "aTargets": [ 8 ] }
-              ';
+            } else if ($type == 'machines') {
+              $headers = array('Name', 'Acronym', 'Manufacturer', 'Owner', 'IPDB', 'Rules', 'Year');
+              $aoColumnDefs = '{"sClass": "icon", "aTargets": [ 5 ] }';
             }
-          } else if ($type == 'machines') {
-            $headers = array('Name', 'Acronym', 'Manufacturer', 'Owner', 'IPDB', 'Rules', 'Year');
-            $aoColumnDefs = '{"sClass": "icon", "aTargets": [ 5 ] }';
-          }
-          foreach ($objs[$division->id] as $obj) {
-            $rows[] = $obj->getRegRow(TRUE);
-          }
-          if ($type == 'players') {
-            $page->addParagraph('<input type="button" id="'.$division->shortName.'_reloadButton" class="reloadButton" value="Reload the table">'.(($division->type == 'main' && config::$participationLimit[$division->type]) ? ' <span class="right">The maximum number of players is '.config::$participationLimit[$division->type].'.</span>' : ''));
-          }
-          $page->addTable($division->shortName.'Table', $headers, $rows, 'regTable');
-          $page->datatables = TRUE;
-          $page->datatablesReload = TRUE;
-          $page->addScript('
-            var tbl = [];
-            tbl["'.$division->shortName.'"] = $("#'.$division->shortName.'Table").dataTable({
-              "bProcessing": true,
-              "bDestroy": true,
-              "bJQueryUI": true,
-          	  "sPaginationType": "full_numbers",
-              '.(($aoColumnDefs) ? '"aoColumnDefs": [
-                '.$aoColumnDefs.'
-              ],' : '').'
-              "fnDrawCallback": function() {
-                $(".photoPopup").each(function() {
-                  $(this).dialog({
-                    autoOpen: false,
-                    modal: true, 
-                    width: "auto",
-                    height: "auto"
+            foreach ($objs[$division->id] as $obj) {
+              $rows[] = $obj->getRegRow(TRUE);
+            }
+            if ($type == 'players') {
+              $page->addParagraph('<input type="button" id="'.$division->shortName.'_reloadButton" class="reloadButton" value="Reload the table">'.(($division->type == 'main' && config::$participationLimit[$division->type]) ? ' <span class="right">The maximum number of players is '.config::$participationLimit[$division->type].'.</span>' : ''));
+            }
+            $page->addTable($division->shortName.'Table', $headers, $rows, 'regTable');
+            $page->datatables = TRUE;
+            $page->datatablesReload = TRUE;
+            $page->addScript('
+              var tbl = [];
+              tbl["'.$division->shortName.'"] = $("#'.$division->shortName.'Table").dataTable({
+                "bProcessing": true,
+                "bDestroy": true,
+                "bJQueryUI": true,
+            	  "sPaginationType": "full_numbers",
+                '.(($aoColumnDefs) ? '"aoColumnDefs": [
+                  '.$aoColumnDefs.'
+                ],' : '').'
+                "fnDrawCallback": function() {
+                  $(".photoPopup").each(function() {
+                    $(this).dialog({
+                      autoOpen: false,
+                      modal: true, 
+                      width: "auto",
+                      height: "auto"
+                    });
                   });
-                });
-                $("#'.$division->shortName.'Table").css("width", "");
-                $(".photoIcon").click(function() {
-                  var photoDiv = $(this).data("photodiv");
-                  $("#" + photoDiv).dialog("open");
-                  $(document).on("click", ".ui-widget-overlay", function() {
-                    $("#" + photoDiv).dialog("close");
+                  $("#'.$division->shortName.'Table").css("width", "");
+                  $(".photoIcon").click(function() {
+                    var photoDiv = $(this).data("photodiv");
+                    $("#" + photoDiv).dialog("open");
+                    $(document).on("click", ".ui-widget-overlay", function() {
+                      $("#" + photoDiv).dialog("close");
+                    });
                   });
-                });
-                return true;
-              },
-              "oLanguage": {
-                "sProcessing": "<img src=\"'.config::$baseHref.'/images/ajax-loader-white.gif\" alt=\"Loading data...\">"
-              },
-              "iDisplayLength": -1,
-              "aLengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]]
-            });
-            '.(($type == 'players') ? '$("#'.$division->shortName.'_reloadButton").click(function() {
-              tbl["'.$division->shortName.'"].fnReloadAjax("'.config::$baseHref.'/ajax/getPlayers.php?type=registered&obj=division&id='.$division->id.'");
-            });' : '').'
-          ');
-        } else {
-          $page->addParagraph('No '.$type.' are registered in the '.strtolower($division->divisionName));
+                  return true;
+                },
+                "oLanguage": {
+                  "sProcessing": "<img src=\"'.config::$baseHref.'/images/ajax-loader-white.gif\" alt=\"Loading data...\">"
+                },
+                "iDisplayLength": -1,
+                "aLengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]]
+              });
+              '.(($type == 'players') ? '$("#'.$division->shortName.'_reloadButton").click(function() {
+                tbl["'.$division->shortName.'"].fnReloadAjax("'.config::$baseHref.'/ajax/getPlayers.php?type=registered&obj=division&id='.$division->id.'");
+              });' : '').'
+            ');
+          } else {
+            $page->addParagraph('No '.$type.' are registered in the '.strtolower($division->divisionName));
+          }
         }
       $page->closeDiv();
     }
