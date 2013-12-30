625f03733bd5192fd27bbfc2ac6295da18c2a3e3
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-30 04:47:09 +0000

    'games'

--- Flipper/EPC/test.europinball.com/classes/base.class.php
+++ Flipper/EPC/test.europinball.com/classes/base.class.php
@@ -213,7 +213,6 @@
     }
 
     public function getInfo() {
-      debug($this);
       $info = new div($this->id.'_'.get_class($this).'_InfoDiv');
       $left = $info->addDiv($this->id.'_'.get_class($this).'_InfoDivLeft', 'left');
       if (static::$infoProps) {
