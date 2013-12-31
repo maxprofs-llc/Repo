c48abe03b996a7f5dbf14a8e7e96dd8c5e8e1123
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-31 04:02:55 +0000

    'games''

--- Flipper/EPC/test.europinball.com/classes/db.class.php
+++ Flipper/EPC/test.europinball.com/classes/db.class.php
@@ -19,6 +19,7 @@
       $sth = $this->action($query, $values);
       if ($sth) {
         $rowCount = $sth->rowCount();
+        debug($rowCount, 'rows');
         return ($rowCount === 0) ? TRUE : $rowCount;
       } else {
         return FALSE;
