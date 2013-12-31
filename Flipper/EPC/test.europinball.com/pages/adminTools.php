e2ba9f0cf22859a0e1f81edd3efa6ac192bb5c40
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2013-12-31 01:27:38 +0000

    'games'´

--- Flipper/EPC/test.europinball.com/pages/adminTools.php
+++ Flipper/EPC/test.europinball.com/pages/adminTools.php
@@ -170,7 +170,7 @@
                   ${$geoClass.'Div'}->addLabel(' ');
                   ${$geoClass.'MergeButton') = ${$geoClass.'Div'}->addButton('Merge', $geoClass.'MergeButton', 'mergeButton');
                   ${$geoClass.'MergeButton')->data-geoclass = $geoClass;
-                  ${$geoClass.'Div'}->addParagraph('Anything now related to the first '.$geoClass.' will be changed to be related to the second '.$geoClass.' when you click the button.', NULL, 'ïtalic');
+                  ${$geoClass.'Div'}->addParagraph('Anything now related to the first '.$geoClass.' will be changed to be related to the second '.$geoClass.' when you click the button.', NULL, 'italic');
                 //}
               }
             //}
