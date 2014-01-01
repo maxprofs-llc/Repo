de95ba1c40e86b99a7a637cf1419f27663b9a0e3
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-01 05:13:58 +0000

    'admin'

--- Flipper/EPC/test.europinball.com/classes/html/html.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/html.class.php
@@ -1137,15 +1137,10 @@
     }
     
     public function getClone($spec = NULL, $id = NULL, $class = NULL, array $params = NULL) {
-      debug($this->contents[0], 'orig');
       $clone = clone $this;
-      debug($clone->contents[0], 'clone1');
       $clone->addParams($params); 
-      debug($clone->contents[0], 'clone2');
       $clone->addClasses($class);
-      debug($clone->contents[0], 'clone3');
       $clone->id = $id;
-      debug($clone->contents[0], 'clone4');
       return $clone;
     }
 
