b63a99096bb741e49f0dea7df243f17b4ec94f70
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-02 11:33:44 +0000

    'admin'

--- Flipper/EPC/europinball.com/classes/tshirtOrders.class.php
+++ Flipper/EPC/europinball.com/classes/tshirtOrders.class.php
@@ -6,6 +6,30 @@
     public static $all = array();
     
     public function __construct($data = NULL, $prop = NULL, $cond = 'and') {
+      if (isTournament($data) || in_array($data, array('active', 'current'))) {
+        $tournament = tournament($data);
+        $context = $tournament;
+        $secondParam = 'prop';
+      }
+      if (isTournament($prop) || in_array($prop, array('active', 'current'))) {
+        $tournament = tournament($prop);
+        $context = $tournament;
+        $secondParam = 'data';
+      } 
+      if ($context) {
+        $class = static::$objClass;
+        if (isObj($$secondParam)) {
+          $secondColumn = (property_exists($$secondParam, 'table')) ? get_class_vars(get_class($$secondParam))['table'].'_id' : get_class($$secondParam).'_id';
+          $secondId = $$secondParam->id;
+        }
+        $data = '
+          where tt.tournamentEdition_id = '.$context->id.'
+            '.((isObj($$secondParam)) ? 'and o.'.$secondColumn.' = '.$secondId : '').'
+            and tt.id is not null
+          group by o.id
+        ';
+        $prop = NULL;
+      }
       parent::__construct($data, $prop, $cond);
     }
     
