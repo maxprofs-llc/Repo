f47d726b0e806bcd5a5899478be64dff3878667d
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-02 04:30:59 +0000

    'admin''

--- Flipper/EPC/test.europinball.com/classes/group.class.php
+++ Flipper/EPC/test.europinball.com/classes/group.class.php
@@ -222,6 +222,7 @@
     }
     
     public function getSelectObj($name = NULL, $selected = NULL, $label = NULL, $params = NULL) {
+      debug($params);
       if (isObj($selected)) {
         $selected = $selected->id;
       } else if (isId($selected)) {
