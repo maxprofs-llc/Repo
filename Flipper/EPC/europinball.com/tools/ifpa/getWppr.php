98b20b08511cda436f73cad94c977b478bf8ab6e
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-02 10:15:48 +0000

    'admin'

--- Flipper/EPC/europinball.com/tools/ifpa/getWppr.php
+++ Flipper/EPC/europinball.com/tools/ifpa/getWppr.php
@@ -6,13 +6,12 @@
   
   $division_id = (isset($_REQUEST['division'])) ? $_REQUEST['division'] : 1;
   $division = division($division_id);
-  debug($division);
   
   $players = players($division);
-  debug($players);
+  $players->order('wpprPlace');
   
   echo "Tournament Name,Date,Finishing Position,Player,Country,IFPA ID\n";
   foreach ($players as $player) {
-    echo 'European Pinball Championships '.(($division == 2) ? 'Classics ' : '').'2013,2013-09-13,'.$place.','.$player->name.','.$player->countryName.','.$player->ifpa_id."\n";
+    echo 'European Pinball Championships '.(($division == 2) ? 'Classics ' : '').'2013,2013-09-13,'.$player->wpprPlace.','.$player->name.','.$player->countryName.','.$player->ifpa_id."\n";
   }
  ?>
