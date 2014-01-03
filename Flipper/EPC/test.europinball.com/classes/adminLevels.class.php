27114bac2c2726902132d8aae818172deee5f305
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-03 03:13:30 +0000

    'admin'

--- Flipper/EPC/test.europinball.com/classes/adminLevels.class.php
+++ Flipper/EPC/test.europinball.com/classes/adminLevels.class.php
@@ -4,6 +4,11 @@
 
     public static $objClass = 'adminLevel';
     public static $all = array();
+    public static $order = array(
+      'prop' => 'id',
+      'type' => 'numeric',
+      'dir' => 'asc'
+    );
 
   }
 
