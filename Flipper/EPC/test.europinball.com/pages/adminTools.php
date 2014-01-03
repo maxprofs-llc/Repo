afaad7e61c7f2678a891c3d3b0554c279baebe94
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-03 13:30:50 +0000

    'admin'

--- Flipper/EPC/test.europinball.com/pages/adminTools.php
+++ Flipper/EPC/test.europinball.com/pages/adminTools.php
@@ -124,7 +124,6 @@
                 ${$prefix.'MailDiv'}[$adminLevel->id]->addH2(ucfirst($adminLevel->name).' email addresses', array('class' => 'entry-title'));
                 ${$prefix.'MailDiv'}[$adminLevel->id]->addParagraph('Email addresses to all volunteers with level '.$adminLevel->id.' or higher that have registered their email address. Click in the box to copy the addresses to your clipboard.');
                 ${$prefix.'MailDiv'}[$adminLevel->id]->addParagraph(implode(', ', $volAddresses[$adminLevel->id]), $prefix.'volAddresses_'.$adminLevel->id, 'toCopy');
-                ${$prefix.'MailDiv'}[$adminLevel->id]->addParagraph('More coming soon...')->style = 'margin-top: 15px';
               //${$prefix.'MailDiv'}
             }
           }
