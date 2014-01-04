0adc806818d99d8457dc88693cf0c41b4d41fb45
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-04 04:21:11 +0000

    'admin'

--- Flipper/EPC/test.europinball.com/classes/game.class.php
+++ Flipper/EPC/test.europinball.com/classes/game.class.php
@@ -116,6 +116,7 @@
               $div->addLabel('Divisions', NULL, NULL, 'normal');
               foreach (config::$activeDivisions as $divisionType) {
                 $division = division($tournament, $divisionType);
+                debug($division);
                 $machine = machine($this, $division);
                 $box[$divisionType] = $div->addCheckbox($divisionType, ($machine), array('id' => $prefix.'Game'.$divisionType, 'class' => $editClass));
               }
