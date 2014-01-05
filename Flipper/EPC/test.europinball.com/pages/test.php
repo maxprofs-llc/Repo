9d7ab93fbfe30377e0bcb19c7835aead536fbad2
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-05 01:18:20 +0000

    'admin'

--- Flipper/EPC/test.europinball.com/pages/test.php
+++ Flipper/EPC/test.europinball.com/pages/test.php
@@ -39,40 +39,40 @@
   $html .= '</thead><tbody>';
   $html .= '<tr>
     <td>$inexistent</td>
-    <td>'.htmlentities(var_export(strlen($var))).'</td>
-    <td>'.htmlentities(var_export(is_null($var))).'</td>
-    <td>'.htmlentities(var_export(isset($var))).'</td>
-    <td>'.htmlentities(var_export(empty($var))).'</td>
-    <td>'.htmlentities(var_export(($var))).'</td>
-    <td>'.htmlentities(var_export(is($var))).'</td>
-    <td>'.htmlentities(var_export(isId($var))).'</td>
-    <td>'.htmlentities(var_export($var == FALSE)).'</td>
-    <td>'.htmlentities(var_export($var === FALSE)).'</td>
-    <td>'.htmlentities(var_export($var == NULL)).'</td>
-    <td>'.htmlentities(var_export($var === NULL)).'</td>
-    <td>'.htmlentities(var_export($var == 0)).'</td>
-    <td>'.htmlentities(var_export($var === 0)).'</td>
-    <td>'.htmlentities(var_export($var == "")).'</td>
-    <td>'.htmlentities(var_export($var === "")).'</td>
+    <td>'.htmlentities(var_export(strlen($var)), TRUE).'</td>
+    <td>'.htmlentities(var_export(is_null($var)), TRUE).'</td>
+    <td>'.htmlentities(var_export(isset($var)), TRUE).'</td>
+    <td>'.htmlentities(var_export(empty($var)), TRUE).'</td>
+    <td>'.htmlentities(var_export(($var)), TRUE).'</td>
+    <td>'.htmlentities(var_export(is($var)), TRUE).'</td>
+    <td>'.htmlentities(var_export(isId($var)), TRUE).'</td>
+    <td>'.htmlentities(var_export($var == FALSE), TRUE).'</td>
+    <td>'.htmlentities(var_export($var === FALSE), TRUE).'</td>
+    <td>'.htmlentities(var_export($var == NULL), TRUE).'</td>
+    <td>'.htmlentities(var_export($var === NULL), TRUE).'</td>
+    <td>'.htmlentities(var_export($var == 0), TRUE).'</td>
+    <td>'.htmlentities(var_export($var === 0), TRUE).'</td>
+    <td>'.htmlentities(var_export($var == ""), TRUE).'</td>
+    <td>'.htmlentities(var_export($var === ""), TRUE).'</td>
   </tr>';
   foreach ($tests as $label => $var) {
     $html .= '<tr>
       <td>'.htmlentities($label).'</td>
-      <td>'.htmlentities(var_export(strlen($var))).'</td>
-      <td>'.htmlentities(var_export(is_null($var))).'</td>
-      <td>'.htmlentities(var_export(isset($var))).'</td>
-      <td>'.htmlentities(var_export(empty($var))).'</td>
-      <td>'.htmlentities(var_export(($var))).'</td>
-      <td>'.htmlentities(var_export(is($var))).'</td>
-      <td>'.htmlentities(var_export(isId($var))).'</td>
-      <td>'.htmlentities(var_export($var == FALSE)).'</td>
-      <td>'.htmlentities(var_export($var === FALSE)).'</td>
-      <td>'.htmlentities(var_export($var == NULL)).'</td>
-      <td>'.htmlentities(var_export($var === NULL)).'</td>
-      <td>'.htmlentities(var_export($var == 0)).'</td>
-      <td>'.htmlentities(var_export($var === 0)).'</td>
-      <td>'.htmlentities(var_export($var == "")).'</td>
-      <td>'.htmlentities(var_export($var === "")).'</td>
+      <td>'.htmlentities(var_export(strlen($var)), TRUE).'</td>
+      <td>'.htmlentities(var_export(is_null($var)), TRUE).'</td>
+      <td>'.htmlentities(var_export(isset($var)), TRUE).'</td>
+      <td>'.htmlentities(var_export(empty($var)), TRUE).'</td>
+      <td>'.htmlentities(var_export(($var)), TRUE).'</td>
+      <td>'.htmlentities(var_export(is($var)), TRUE).'</td>
+      <td>'.htmlentities(var_export(isId($var)), TRUE).'</td>
+      <td>'.htmlentities(var_export($var == FALSE), TRUE).'</td>
+      <td>'.htmlentities(var_export($var === FALSE), TRUE).'</td>
+      <td>'.htmlentities(var_export($var == NULL), TRUE).'</td>
+      <td>'.htmlentities(var_export($var === NULL), TRUE).'</td>
+      <td>'.htmlentities(var_export($var == 0), TRUE).'</td>
+      <td>'.htmlentities(var_export($var === 0), TRUE).'</td>
+      <td>'.htmlentities(var_export($var == ""), TRUE).'</td>
+      <td>'.htmlentities(var_export($var === ""), TRUE).'</td>
     </tr>';
   }
   
