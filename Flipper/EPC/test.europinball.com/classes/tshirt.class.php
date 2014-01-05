9df4ec63d2ba0bad45f9a9f81d1d9082257c4999
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-05 18:47:52 +0000

    admin

--- Flipper/EPC/test.europinball.com/classes/tshirt.class.php
+++ Flipper/EPC/test.europinball.com/classes/tshirt.class.php
@@ -38,7 +38,7 @@
     public function __construct($data = NULL, $search = config::NOSEARCH, $depth = NULL) {
       parent::__construct($data, $search, $depth);
       $tshirtOrders = tshirtOrders($this);
-      debug(count($tshirtOrders), 'count');
+      debug(count($tshirtOrders), 'count '.$this->id);
       $this->reservers = count($tshirtOrders->getAllOf('person_id'));
       $this->reserved = array_sum($tshirtOrders->getAllOf('number'));
       $this->delivered = array_sum($tshirtOrders->getAllOf('numberDelivered'));
