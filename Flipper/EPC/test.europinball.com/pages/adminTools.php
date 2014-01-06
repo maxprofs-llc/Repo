0d5aaf1eb6d2593f0be485f63a9df59ba30a5b5c
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-06 04:29:19 +0000

    admin

--- Flipper/EPC/test.europinball.com/pages/adminTools.php
+++ Flipper/EPC/test.europinball.com/pages/adminTools.php
@@ -331,7 +331,7 @@
           //$tshirtsMoreNumDiv
           $tshirtsNumDiv = $tshirtsDiv->addDiv();
             $tshirtsNumDiv->addLabel('Estimated number');
-            $tshirtsNum = (+ $paymentsSum - $paymentsSum * 30);
+            $tshirtsNum = $paymentsSum - $paymentsSum * 30;
             $tshirtsNumDiv->addSpan('At least '.$tshirtsNum.' T-shirts'); 
           //$tshirtsNumDiv
           $tshirts = tshirts($tournament);
