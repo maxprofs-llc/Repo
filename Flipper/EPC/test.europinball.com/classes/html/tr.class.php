85725cbe67d5c5872c27eb587282c5d0fea162a3
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-06 02:23:27 +0000

    admin

--- Flipper/EPC/test.europinball.com/classes/html/tr.class.php
+++ Flipper/EPC/test.europinball.com/classes/html/tr.class.php
@@ -11,13 +11,13 @@
 //    html public function __construct($element = 'span', $contents = NULL, array $params = NULL, $id = NULL, $class = NULL, array $css = NULL, $indents = 0) {
     
     public function addContent($content = NULL, $replace = FALSE, $index = FALSE) {
+      debug($content);
       $class = ($this->type == 'thead') ? 'th' : 'td';
       if (is_array($content)) {
         if ($replace) {
           $this->delContent($replace);
         }
         foreach ($content as $part) {
-          debug($part);
           $this->addContent($part, NULL, $index);
           $index++;
         }
