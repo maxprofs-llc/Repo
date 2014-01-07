4ee42a3e3d73f59f62fa5500b08061f85c86f24f
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-07 20:36:33 +0000

    admin

--- Flipper/EPC/test.europinball.com/pages/adminTools.php
+++ Flipper/EPC/test.europinball.com/pages/adminTools.php
@@ -64,18 +64,21 @@
               $waitingDiv->addLabel($division->divisionName.':');
               $waitingButton = $waitingDiv->addButton('Recalculate waiting list', $division->id.'calcWaiting');
               $waitingButton->addTooltip('');
+              $waitingButton->addCss('margin-top', '15px');
+              $waitingDiv->addLabel('Number of players:');
+              $calcSpan = $waitingDiv->addSpan($players->getNumOf('waiting'));
+              $waitingDiv->addSpan(' players');
               $waitingButton->addClick('
                 var el = this;
                 $(el).tooltipster("update", "Recalculating waiting list...").tooltipster("show");
                 $.post("'.config::$baseHref.'/ajax/calcWaiting.php", {division_id: '.$division->id.'})
                 .done(function(data) {
                   $(el).tooltipster("update", data.reason).tooltipster("show");
+                  if (data.valid) {
+                    $("#'.$calcSpan->id.'").html(data.number);
+                  }
                 }) 
               ');
-              $waitingButton->addCss('margin-top', '15px');
-              $waitingDiv->addLabel('Number of players:');
-              $waitingDiv->addSpan($players->getNumOf('waiting'));
-              $waitingDiv->addSpan(' players');
             //$waitingDiv
           }
           $personMailAddresses = $persons->getListOf('mailAddress');
