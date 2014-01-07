82fbdbaa3771b336d4f379e188635d92d12f4dcf
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-07 09:06:55 +0000

    admin4

--- Flipper/EPC/test.europinball.com/classes/person.class.php
+++ Flipper/EPC/test.europinball.com/classes/person.class.php
@@ -374,7 +374,7 @@
           foreach ($players as $player) {
             $div = $adminDiv->addDiv();
             $checkbox = $div->addCheckbox('adminNoWaiting'.$player->tournamentDivision->shortName, $player->noWaiting, array('class' => 'nowaiting'));
-            $checkbox->label = 'Excepted from list'
+            $checkbox->label = 'Excepted from list';
             $div->addHidden('adminNoWaiting'.$player->tournamentDivision->shortName.'PlayerId', $player->id);
             $div->addHidden('adminNoWaiting'.$player->tournamentDivision->shortName.'DivisionId', $player->tournamentDivision_id);
             $div->addLabel('Current place:');
