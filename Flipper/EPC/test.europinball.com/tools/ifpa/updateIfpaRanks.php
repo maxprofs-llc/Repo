96e84f6aa7438fd6cdd0671f359dd2cbe2b216d2
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-08 07:01:18 +0000

    admin

--- Flipper/EPC/test.europinball.com/tools/ifpa/updateIfpaRanks.php
+++ Flipper/EPC/test.europinball.com/tools/ifpa/updateIfpaRanks.php
@@ -9,23 +9,22 @@
   $query = 'select id, firstName, lastName, ifpa_id, ifpaRank from person where ifpa_id is not null and ifpaUpdateReq = 1';
   $persons = base::$_db->select($query, NULL, 'ifpaPerson');
 //    @apache_setenv('no-gzip', 1);
-    @ini_set('zlib.output_compression', 0);
-    @ini_set('implicit_flush', 1);
-  
-    foreach ($persons as $person) {
-      echo '<pre>';
-      var_dump($person);
-      $rank = get_rank_from_id($person->ifpa_id);
-      echo 'Found rank: '.$rank['rank'];
-      echo 'Setting rank to: '.(($rank['rank'] != -1) ? $rank['rank'] : 0);
-      if ($rank['rank'] || $rank['rank'] === 0) {
-        $person->updateRank((($rank['rank'] != -1) ? $rank['rank'] : 0));
-      }
-      echo '</pre>';
-      $p++;
-      for ($i = 0; $i < ob_get_level(); $i++) { ob_end_flush(); }
-      ob_implicit_flush(1);
+  @ini_set('zlib.output_compression', 0);
+  @ini_set('implicit_flush', 1);
+
+  foreach ($persons as $person) {
+    echo '<pre>';
+    var_dump($person);
+    $rank = get_rank_from_id($person->ifpa_id);
+    echo 'Found rank: '.$rank['rank'];
+    echo 'Setting rank to: '.(($rank['rank'] != -1) ? $rank['rank'] : 0);
+    if ($rank['rank'] || $rank['rank'] === 0) {
+      $person->updateRank((($rank['rank'] != -1) ? $rank['rank'] : 0));
     }
+    echo '</pre>';
+    $p++;
+    for ($i = 0; $i < ob_get_level(); $i++) { ob_end_flush(); }
+    ob_implicit_flush(1);
   }
   
   getIfpaPlayers($dbh, true);
