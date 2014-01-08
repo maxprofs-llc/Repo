d5a1601d88cd856964b67bd5d2d36e7c139b59d1
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-08 05:16:25 +0000

    admin

--- Flipper/EPC/test.europinball.com/tools/ifpa/getIfpaPlayers.php
+++ Flipper/EPC/test.europinball.com/tools/ifpa/getIfpaPlayers.php
@@ -1,18 +1,14 @@
 <?php
   define('__ROOT__', dirname(dirname(dirname(__FILE__)))); 
-  require_once(__ROOT__.'/functions/general.php');
-  require_once(__ROOT__.'/tools/ifpa/ifparank.php');
+  require_once(__ROOT__.'/functions/init.php');
   
-  function getIfpaPlayers($dbh, $update) {
-    $where = 'where p.ifpa_id is not null';
-    $where .= ($update) ? ' and p.ifpaRank is null' : null;
-      
-    $objs = getIfpaIds($dbh, $where);
+  $persons = persons('where o.ifpa_id is not null and ifnull(password, 0) != "checked"');
   
 //    @apache_setenv('no-gzip', 1);
     @ini_set('zlib.output_compression', 0);
     @ini_set('implicit_flush', 1);
   
+/*
     foreach ($objs as $obj) {
       echo '<pre>';
       $rank = get_rank_from_id($obj->ifpa_id);
@@ -29,5 +25,8 @@
   }
   
   getIfpaPlayers($dbh, true);
+*/
+
+debug($persons);
   
 ?>
