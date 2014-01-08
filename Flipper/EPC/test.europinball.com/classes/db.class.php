04200e8674ec98e218edc87521f925c69e3a4b2b
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-08 05:56:07 +0000

    admin

--- Flipper/EPC/test.europinball.com/classes/db.class.php
+++ Flipper/EPC/test.europinball.com/classes/db.class.php
@@ -160,6 +160,7 @@
 
     public function getObjectsByWhere($class, $where = 1) {
       $query = $class::$select.' '.((preg_match('/^where /', trim($where))) ? '' : ((preg_match('/^left join /', trim($where))) ? '' : 'where ')).$where;
+      debug($query);
       $sth = $this->query($query);
       if (!$sth) {
         return FALSE;
