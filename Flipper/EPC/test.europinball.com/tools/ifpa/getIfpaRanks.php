edf6f6738e1149df3554b0fca4677f9ce50e396d
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-08 06:41:14 +0000

    admin

--- Flipper/EPC/test.europinball.com/tools/ifpa/getIfpaRanks.php
+++ Flipper/EPC/test.europinball.com/tools/ifpa/getIfpaRanks.php
@@ -6,28 +6,28 @@
   if (!base::$_db) {
     base::$_db = new db();
   }
-  $query = 'select id, firstName, lastName, ifpa_id, ifpaRank from person where ifpa_id is not null and ifnull(password, 0) != "checked"';
-  $objs = base::$_db->select($query, NULL, 'ifpaPerson');
-/*
+  $query = 'select id, firstName, lastName, ifpa_id, ifpaRank from person where ifpa_id is not null and ifpaUpdateReq = 1';
+  $persons = base::$_db->select($query, NULL, 'ifpaPerson');
+
 //    @apache_setenv('no-gzip', 1);
     @ini_set('zlib.output_compression', 0);
     @ini_set('implicit_flush', 1);
   
-    foreach ($objs as $obj) {
+    foreach ($persons as $person) {
       echo '<pre>';
-      $rank = get_rank_from_id($obj->ifpa_id);
-      var_dump($obj);
+      var_dump($person);
+      $rank = get_rank_from_id($person->ifpa_id);
       echo 'Found rank: '.$rank['rank'];
       echo 'Setting rank to: '.(($rank['rank'] != -1) ? $rank['rank'] : 0);
-      $obj->rank = ($rank['rank'] != -1) ? $rank['rank'] : 0;
-      var_dump($obj);
+      $person->rank = ($rank['rank'] != -1) ? $rank['rank'] : 0;
+      var_dump($person);
       echo '</pre>';
       $p++;
       for ($i = 0; $i < ob_get_level(); $i++) { ob_end_flush(); }
       ob_implicit_flush(1);
-      if ($obj->rank || $obj->rank === 0) {
-        $person = 
-        updateIfpaRank($dbh, $obj);
+      if ($person->rank || $person->rank === 0) {
+        $person->setProp('ifpaRank', $person->rank);
+        $person->setProp('ifpaUpdateReq', $person->rank);
       }
     }
   }
