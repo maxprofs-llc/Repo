4c26dcf18308d71989f3d527a6feff366ba47e1e
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-09 18:31:44 +0000

    admin

--- Flipper/EPC/test.europinball.com/classes/division.class.php
+++ Flipper/EPC/test.europinball.com/classes/division.class.php
@@ -123,6 +123,7 @@
       debug($query);
       $return = $this->update($query);
       if ($return) {
+        return $return;
         $query = 'select max(waiting) from player where tournamentDivision_id = '.$this->id;
         return $this->db->getValue($query);
       } else {
