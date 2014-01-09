b6e8ffba198bbfcdf6df1aa5df3557fee5c27f21
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-09 02:02:46 +0000

    admin

--- Flipper/EPC/test.europinball.com/classes/person.class.php
+++ Flipper/EPC/test.europinball.com/classes/person.class.php
@@ -501,7 +501,7 @@
                 $usersNewUsernameSpan = $userNameDiv->addSpan('No username found! ', $prefix.'usersNewUsernameSpan');
                 $usersNewUsernameButton = $usersNewUsernameSpan->addButton('Add login credentials');
                 $newUserDialogDiv = $userNameDiv->addDiv();
-                $newUserDialogDiv->addContent(page::getNewUser($title = 'Add login for '.$this->name, $this->id, NULL, NULL, TRUE, $autoopen = FALSE));
+                $newUserDialogDiv->addContent(page::getNewUser($title = 'Add login for '.$this->name, $this->id, NULL, 'adminUsers', TRUE, $autoopen = FALSE));
                 $newUserDialogDiv->escape = FALSE; 
                 $usersNewUsernameButton->addClick('
                   $("#newUserDiv").dialog("open");
