013ffa28748a78312666ecb8b56cfa903436035c
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-01-17 10:29:37 +0000

    team admin

--- Flipper/EPC/test.europinball.com/classes/team.class.php
+++ Flipper/EPC/test.europinball.com/classes/team.class.php
@@ -194,12 +194,9 @@
           $membersDiv->addH2('Team members', array('class' => 'entry-title'));
           $members = persons($this, $tournament);
           $player = player($this, $tournament);
-          debug($player);
           $division = division($player);
-          debug($division);
           $persons = ($this->national) ? persons($tournament, $this->country) : persons($tournament);
-          debug($this->tournamentDivision->teamMembers); 
-          for ($i = 0; $i < $this->tournamentDivision->teamMembers; $i++) {
+          for ($i = 0; $i < $division->teamMembers; $i++) {
             ${$prefix.$i.'SelectDiv'} = $membersDiv->addDiv();
               ${$prefix.$i.'Select'} = ${$prefix.$i.'SelectDiv'}->addContent($persons->getSelectObj($prefix.$i.'member', $members[$i]->id, 'member', array('class' => $comboboxClass)));
               ${$prefix.$i.'Select'}->addCombobox();
