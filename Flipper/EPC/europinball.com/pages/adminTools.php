9fe07cdef5f8e50cd81478bdf82ba5a4a307c027
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-02-25 23:06:40 +0000

    mail

--- Flipper/EPC/europinball.com/pages/adminTools.php
+++ Flipper/EPC/europinball.com/pages/adminTools.php
@@ -82,7 +82,7 @@
             //$waitingDiv
           }
           $personMailAddresses = $persons->getListOf('mailAddress');
-          $mainPlayers = players($persons, division('main'));
+          $mainPlayers = players('main');
           $inTournamentPersons = $mainPlayers->getFiltered('waiting', 0, '>', TRUE);
           $inTournamentMailAddresses = $inTournamentPersons->getListOf('mailAddress'); 
           if ($personMailAddresses || $inTournamentMailAddresses) {
