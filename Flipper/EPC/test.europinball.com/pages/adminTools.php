d65fe57ba043be9863126bc80adf812d080de3b2
pal <pal@b71672e9-06c6-e211-b69f-005056ae6b28>
2014-03-09 02:20:25 +0000

    adminTools

--- Flipper/EPC/test.europinball.com/pages/adminTools.php
+++ Flipper/EPC/test.europinball.com/pages/adminTools.php
@@ -9,6 +9,7 @@
 
   if ($page->reqLogin('You need to be logged in to access this page. If you don\'t have a user, please go to the <a href="'.config::$baseHref.'/registration/">registration page</a>.')) {
     $adminDiv = new div('adminDiv');
+    $volunteer = volunteer('login');
     if ($volunteer->receptionist) {
       $tabs = $adminDiv->addTabs(NULL, 'adminTabs');
         $adminTabs = array('players', 'users', 'payments', 'teams', 'groups', 'games', 'scores', 'results', 'volunteers', 'tshirts', 'other');
